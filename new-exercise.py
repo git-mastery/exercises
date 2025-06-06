5900428e5117bea983f5e91e3236331007c603f0
Jiahao, Woo <woojiahao1234@gmail.com>
2025-06-06 11:29:50 +0800

    Autogen more files with new-exercise

--- new-exercise.py
+++ new-exercise.py
@@ -56,11 +56,65 @@ def main():
         download_script = """
         #!/bin/bash
 
-        # Specify the downloaded behavior
+        first_commit_hash=$(git log --reverse --pretty=format:'%h' | head -n 1)
+        if [ $(git tag -l "git-mastery-start-$first_commit_hash") ]; then
+          git tag -d "git-mastery-start-$first_commit_hash"
+        fi
+
+        git tag "git-mastery-start-$first_commit_hash"
         """
         download_script_file.write(textwrap.dedent(download_script).lstrip())
 
-    os.makedirs(exercise_dir / "res")
+    with open(exercise_dir / "verify.py", "w") as verify_script_file:
+        verify_script = """
+        from typing import List
+
+        from git_autograder import GitAutograderOutput, GitAutograderRepo
+
+        def verify(repo: GitAutograderRepo) -> GitAutograderOutput:
+            comments: List[str] = []
+
+            # INSERT YOUR GRADING CODE HERE
+
+            return repo.to_output(comments)
+        """
+        verify_script_file.write(textwrap.dedent(verify_script).lstrip())
+
+    open(exercise_dir / "__init__.py", "a").close()
+
+    os.makedirs(exercise_dir / "res", exist_ok=True)
+
+    tests_dir = exercise_dir / "tests"
+    os.makedirs(tests_dir, exist_ok=True)
+
+    with open(tests_dir / "test_verify.py", "w") as test_grade_file:
+        test_grade = f"""
+        from git_autograder import GitAutograderTestLoader
+
+        from ..verify import verify
+
+        REPOSITORY_NAME = "{exercise_name}"
+
+        loader = GitAutograderTestLoader(REPOSITORY_NAME, verify)
+
+
+        def test():
+            with loader.load("specs/base.yml", "start"):
+                pass
+        """
+        test_grade_file.write(textwrap.dedent(test_grade).lstrip())
+
+    os.makedirs(tests_dir / "specs", exist_ok=True)
+    with open(tests_dir / "specs" / "base.yml", "w") as base_spec_file:
+        base_spec = """
+        initialization:
+          steps:
+            - type: commit
+              empty: true
+              message: Empty commit
+              id: start
+        """
+        base_spec_file.write(textwrap.dedent(base_spec).lstrip())
 
 
 if __name__ == "__main__":
