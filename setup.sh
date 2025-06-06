46f15f317c29cb721ab5ea53f3633e2cdfd5c634
Jiahao, Woo <woojiahao1234@gmail.com>
2025-06-06 13:04:43 +0800

    Remove deactivation logic in setup

--- setup.sh
+++ setup.sh
@@ -4,10 +4,6 @@ if [ ! -d venv/ ]; then
   python -m venv venv
 fi
 
-if [[ $VIRTUAL_ENV != "" ]]; then
-  deactivate
-fi
-
 source venv/bin/activate
 
-pip install -r requirements.txt
+pip install -r requirements.txt -U --no-cache-dir
