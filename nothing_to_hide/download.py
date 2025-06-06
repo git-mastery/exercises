f6a8f348823cb475dd333829502e875d4d0738f6
Jiahao, Woo <woojiahao1234@gmail.com>
2025-06-06 21:18:32 +0800

    Add download instructions for nothing-to-hide

--- nothing_to_hide/download.py
+++ nothing_to_hide/download.py
@@ -1,4 +1,7 @@
+import os
+from pathlib import Path
 import subprocess
+import textwrap
 from typing import List, Optional
 
 
@@ -19,7 +22,45 @@ def run_command(command: List[str], verbose: bool) -> Optional[str]:
         exit(1)
 
 
+def create_file(file_name: str, contents: str) -> None:
+    os.makedirs(Path(file_name).parent, exist_ok=True)
+    with open(file_name, "w") as file:
+        file.write(textwrap.dedent(contents).strip())
+
+
 def setup(verbose: bool = False):
+    create_file("src/script.py", 'print("hello world!")')
+    create_file(
+        "src/.env",
+        """
+        KEY=secretshhh
+        KEY=secretshhh
+        """,
+    )
+    create_file(
+        "sensitive/names.txt",
+        """
+        John
+        Alice
+        Bob
+        Michael
+        """,
+    )
+    for i in range(1, 6):
+        create_file(f"sensitive/sensitive_{i}.txt", "")
+
+    create_file(
+        ".gitignore",
+        """
+        sensitive/*
+        res/hidden.png
+        src/.env
+        !sensitive/names.txt
+        """,
+    )
+    run_command(["git", "add", ".gitignore"], verbose)
+    run_command(["git", "commit", "-m", "Add files"], verbose)
+
     commits_str = run_command(
         ["git", "log", "--reverse", "--pretty=format:%h"], verbose
     )
