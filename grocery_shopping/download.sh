c3c45f122269b79c714f9c7d099c791af19038fb
Jiahao, Woo <woojiahao1234@gmail.com>
2025-06-06 21:22:49 +0800

    Add download scripts

--- grocery_shopping/download.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-first_commit_hash=$(git log --reverse --pretty=format:'%h' | head -n 1)
-if [ $(git tag -l "git-mastery-start-$first_commit_hash") ]; then
-  git tag -d "git-mastery-start-$first_commit_hash"
-fi
-
-git tag "git-mastery-start-$first_commit_hash"
