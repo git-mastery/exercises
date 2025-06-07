9c0d815cb87201c0e9e53642b1a88d5d49d93cad
Jiahao, Woo <woojiahao1234@gmail.com>
2025-06-07 21:45:54 +0800

    Update verification

--- stage_fright/verify.py
+++ stage_fright/verify.py
@@ -6,6 +6,9 @@ from git_autograder import GitAutograderOutput, GitAutograderRepo
 def verify(repo: GitAutograderRepo) -> GitAutograderOutput:
     comments: List[str] = []
 
-    # INSERT YOUR GRADING CODE HERE
+    staged_diff = repo.repo.index.diff("HEAD")
+
+    for diff_item in staged_diff:
+        print(diff_item.a_path)
 
     return repo.to_output(comments)
