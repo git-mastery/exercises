8240aa73544ea55954b5888e2d06410dcfc8ed92
Jiahao, Woo <woojiahao1234@gmail.com>
2025-06-07 21:51:05 +0800

    Flesh out verification

--- stage_fright/verify.py
+++ stage_fright/verify.py
@@ -3,12 +3,20 @@ from typing import List
 from git_autograder import GitAutograderOutput, GitAutograderRepo
 
 
-def verify(repo: GitAutograderRepo) -> GitAutograderOutput:
-    comments: List[str] = []
+EXPECTED_FILES = {"alice.txt", "bob.txt", "jim.txt", "joe.txt", "carrey.txt"}
 
+
+def verify(repo: GitAutograderRepo) -> GitAutograderOutput:
     staged_diff = repo.repo.index.diff("HEAD")
 
+    added_files = set()
     for diff_item in staged_diff:
-        print(diff_item.a_path)
+        added_files.add(diff_item.a_path)
 
-    return repo.to_output(comments)
+    if len(added_files & EXPECTED_FILES) != len(EXPECTED_FILES):
+        missing_files = EXPECTED_FILES.difference(added_files)
+        raise repo.wrong_answer([f"Did not add {file}" for file in missing_files])
+
+    return repo.to_output(
+        ["Great work! You have successfully checked in all the actors!"]
+    )
