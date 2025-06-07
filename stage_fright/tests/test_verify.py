1a33ccff636a040a86f96608a8bf101c75a3feea
Jiahao, Woo <woojiahao1234@gmail.com>
2025-06-08 07:54:04 +0800

    Add unit tests for stage_fright

--- stage_fright/tests/test_verify.py
+++ stage_fright/tests/test_verify.py
@@ -1,12 +1,20 @@
-from git_autograder import GitAutograderTestLoader
+from git_autograder import GitAutograderStatus, GitAutograderTestLoader
 
-from ..verify import verify
+from ..verify import NOT_ADDED, verify
 
 REPOSITORY_NAME = "stage-fright"
 
 loader = GitAutograderTestLoader(__file__, REPOSITORY_NAME, verify)
 
 
-def test():
-    with loader.load("specs/base.yml", "start"):
-        pass
+def test_missing_add():
+    with loader.load("specs/missing_add.yml", "start") as output:
+        assert output.status == GitAutograderStatus.UNSUCCESSFUL
+        names = ["alice", "bob", "joe"]
+        for name in names:
+            assert NOT_ADDED.format(file=f"{name}.txt") in (output.comments or [])
+
+
+def test_added_all():
+    with loader.load("specs/added_all.yml", "start") as output:
+        assert output.status == GitAutograderStatus.SUCCESSFUL
--- stage_fright/verify.py
+++ stage_fright/verify.py
@@ -2,6 +2,8 @@ from git_autograder import GitAutograderOutput, GitAutograderRepo, GitAutograder
 
 EXPECTED_FILES = {"alice.txt", "bob.txt", "jim.txt", "joe.txt", "carrey.txt"}
 
+NOT_ADDED = "Did not add {file}"
+
 
 def verify(repo: GitAutograderRepo) -> GitAutograderOutput:
     staged_diff = repo.repo.index.diff("HEAD")
@@ -12,7 +14,7 @@ def verify(repo: GitAutograderRepo) -> GitAutograderOutput:
 
     if len(added_files & EXPECTED_FILES) != len(EXPECTED_FILES):
         missing_files = EXPECTED_FILES.difference(added_files)
-        raise repo.wrong_answer([f"Did not add {file}" for file in missing_files])
+        raise repo.wrong_answer([NOT_ADDED.format(file=file) for file in missing_files])
 
     return repo.to_output(
         ["Great work! You have successfully checked in all the actors!"],
