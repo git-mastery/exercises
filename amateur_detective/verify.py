48b684776ce876f3aef8956826b57ab1e7a60219
Jiahao, Woo <woojiahao1234@gmail.com>
2025-06-08 14:57:56 +0800

    Update files

--- amateur_detective/verify.py
+++ amateur_detective/verify.py
@@ -1,11 +1,17 @@
-from typing import List
-
 from git_autograder import GitAutograderOutput, GitAutograderRepo
+from git_autograder.answers.rules import HasExactValueRule, NotEmptyRule
+
+QUESTION_ONE = "Which file was added?"
+QUESTION_TWO = "Which file was edited?"
 
 
 def verify(repo: GitAutograderRepo) -> GitAutograderOutput:
-    comments: List[str] = []
+    repo.answers.validate_question(
+        QUESTION_ONE,
+        [
+            NotEmptyRule(),
+            HasExactValueRule("file77.txt"),
+        ],
+    ).validate_question(QUESTION_TWO, [NotEmptyRule(), HasExactValueRule("file14.txt")])
 
-    # INSERT YOUR GRADING CODE HERE
-
-    return repo.to_output(comments)
+    return repo.to_output([])
