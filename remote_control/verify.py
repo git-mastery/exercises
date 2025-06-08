7313a1dcb204695f3e29999ef4f98ce446dc891f
Jiahao, Woo <woojiahao1234@gmail.com>
2025-06-08 22:04:33 +0800

    Update verify

--- remote_control/verify.py
+++ remote_control/verify.py
@@ -1,16 +1,29 @@
-from typing import List
+import subprocess
+from datetime import datetime
 
+import pytz
 from git_autograder import GitAutograderOutput, GitAutograderStatus
 
 
 def verify() -> GitAutograderOutput:
-    res = input("hi: ")
-    print(res)
-
-    return GitAutograderOutput(
-        status=GitAutograderStatus.SUCCESSFUL,
-        started_at=None,
-        completed_at=None,
-        comments=[],
-        exercise_name="remote-control",
-    )
+    started_at = datetime.now(tz=pytz.UTC)
+    url = input("Enter the url of your remote repository: ")
+    code = subprocess.call(["git", "ls-remote", url, "--quiet"])
+    if code == 0:
+        return GitAutograderOutput(
+            status=GitAutograderStatus.SUCCESSFUL,
+            started_at=started_at,
+            completed_at=datetime.now(tz=pytz.UTC),
+            comments=["Great work setting up a public remote repository!"],
+            exercise_name="remote-control",
+        )
+    else:
+        return GitAutograderOutput(
+            status=GitAutograderStatus.UNSUCCESSFUL,
+            started_at=started_at,
+            completed_at=datetime.now(tz=pytz.UTC),
+            comments=[
+                "The remote repository url you provided either does not exist, or is private. Try again!"
+            ],
+            exercise_name="remote-control",
+        )
