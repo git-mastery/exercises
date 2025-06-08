83f5015b60b87149668892d182dcbad6004b7668
Jiahao, Woo <woojiahao1234@gmail.com>
2025-06-08 21:49:37 +0800

    Add remote repo exercise

---
+++ remote_control/tests/test_verify.py
@@ -0,0 +1,12 @@
+from git_autograder import GitAutograderTestLoader
+
+from ..verify import verify
+
+REPOSITORY_NAME = "remote-control"
+
+loader = GitAutograderTestLoader(__file__, REPOSITORY_NAME, verify)
+
+
+def test():
+    with loader.load("specs/base.yml", "start"):
+        pass
