2981d5a18a47a82ce0f13e11185a79cc1a68e17c
woojiahao <woojiahao@users.noreply.github.com>
2025-06-19 02:40:55 +0000

    deploy: 21ac308eb06e804f40170a0f4c56253084f7e716

--- push_over/verify.py
+++ push_over/verify.py
@@ -1,12 +1,16 @@
 import os
 import subprocess
-from typing import List, Optional
 from datetime import datetime
-import pytz
+from typing import List, Optional
 
+import pytz
 from git_autograder import GitAutograderOutput, GitAutograderRepo, GitAutograderStatus
 
 MISSING_REPO = "You should have {repo} in your exercise folder. You might want to re-download the exercise."
+MISSING_COMMIT = "You should have made a separate commit!"
+MISSING_COMMIT_REMOTE = (
+    "You might have forgotten to push your commit to the remote repository."
+)
 
 
 def run_command(command: List[str], verbose: bool) -> Optional[str]:
@@ -27,14 +31,10 @@ def run_command(command: List[str], verbose: bool) -> Optional[str]:
 
 
 def verify() -> GitAutograderOutput:
-    comments: List[str] = []
     started_at = datetime.now(tz=pytz.UTC)
 
     username = run_command(["gh", "api", "user", "-q", ".login"], verbose=False)
     repo_name = f"{username}-gitmastery-push-over"
-
-    print(os.getcwd())
-    print(os.listdir())
 
     if not os.path.isdir(repo_name):
         return GitAutograderOutput(
@@ -46,4 +46,18 @@ def verify() -> GitAutograderOutput:
 
     repo = GitAutograderRepo("push-over", repo_name)
 
-    return repo.to_output(comments)
+    if repo.commits == 1:
+        raise repo.wrong_answer([MISSING_COMMIT])
+
+    origin_remote = repo.remotes.remote("origin")
+    origin_remote.remote.fetch()
+    remote_branch = repo.repo.refs["origin/main"]
+    remote_commits = list(repo.repo.iter_commits(remote_branch))
+
+    if len(remote_commits) == 1:
+        raise repo.wrong_answer([MISSING_COMMIT_REMOTE])
+
+    return repo.to_output(
+        ["Great work pushing changes to the remote!"],
+        status=GitAutograderStatus.SUCCESSFUL,
+    )
