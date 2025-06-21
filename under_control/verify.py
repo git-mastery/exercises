0dd70dd40c35c9cd0ed3594ece9aacea5cd749fe
Jiahao, Woo <woojiahao1234@gmail.com>
2025-06-22 00:59:54 +0800

    [under-control] Use GitAutograderExercise

--- under_control/verify.py
+++ under_control/verify.py
@@ -1,31 +1,19 @@
-from datetime import datetime
 from pathlib import Path
 
-import pytz
-from git_autograder import GitAutograderOutput
+from git_autograder import GitAutograderExercise, GitAutograderOutput
 from git_autograder.status import GitAutograderStatus
 
 
-def verify() -> GitAutograderOutput:
-    started_at = datetime.now(tz=pytz.UTC)
-
+def verify(exercise: GitAutograderExercise) -> GitAutograderOutput:
     current = Path.cwd()
     for parent in [current] + list(current.parents):
         if (parent / ".gitmastery-exercise.json").is_file():
             if (parent / ".git").is_dir():
-                return GitAutograderOutput(
-                    status=GitAutograderStatus.SUCCESSFUL,
-                    started_at=started_at,
-                    completed_at=datetime.now(tz=pytz.UTC),
-                    comments=[
+                return exercise.to_output(
+                    [
                         "You successfully used git init to initialize this folder as a Git repository!"
                     ],
-                    exercise_name="under-control",
+                    GitAutograderStatus.SUCCESSFUL,
                 )
-    return GitAutograderOutput(
-        status=GitAutograderStatus.UNSUCCESSFUL,
-        started_at=started_at,
-        completed_at=datetime.now(tz=pytz.UTC),
-        comments=["This folder isn't a Git repository yet!"],
-        exercise_name="under-control",
-    )
+
+    raise exercise.wrong_answer(["This folder isn't a Git repository yet!"])
