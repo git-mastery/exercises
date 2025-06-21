7d7696f8c4c00cfa1ffd06d87e1674ae32a5228a
woojiahao <woojiahao@users.noreply.github.com>
2025-06-21 16:55:37 +0000

    deploy: c844bed193d2a3318b8987dd855d32ad4da9fab9

--- remote_control/download.py
+++ remote_control/download.py
@@ -3,9 +3,7 @@ import shutil
 
 
 def setup(verbose: bool = False):
-    print(os.getcwd())
+    # TODO: Resolve this bug where the assert doesn't happen but the folder still exists
     os.chdir("..")
-    print(os.listdir())
-    print(os.getcwd())
     shutil.rmtree(f"{os.getcwd()}/deleted", ignore_errors=False)
     assert not os.path.exists("deleted"), "Still exists after rmtree!"
