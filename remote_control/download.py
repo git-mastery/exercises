0ffbeaaea6d00be99d467359034a999987ed87bf
woojiahao <woojiahao@users.noreply.github.com>
2025-06-21 16:51:18 +0000

    deploy: feb24aa0df2f65634b19b1e7e8e29284c88e3312

--- remote_control/download.py
+++ remote_control/download.py
@@ -6,4 +6,4 @@ def setup(verbose: bool = False):
     print(os.getcwd())
     os.chdir("..")
     print(os.listdir())
-    shutil.rmtree(f"{os.getcwd()}/deleted")
+    shutil.rmtree(f"{os.getcwd()}/deleted", ignore_errors=False)
