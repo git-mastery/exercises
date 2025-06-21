92797a2cea64e723d3fee849121d056871b874b4
Jiahao, Woo <woojiahao1234@gmail.com>
2025-06-22 00:16:10 +0800

    [push-over] Update verify to use GitAutograderExercise

--- push_over/verify.py
+++ push_over/verify.py
@@ -1,10 +1,12 @@
 import os
 import subprocess
-from datetime import datetime
 from typing import List, Optional
 
-import pytz
-from git_autograder import GitAutograderOutput, GitAutograderRepo, GitAutograderStatus
+from git_autograder import (
+    GitAutograderExercise,
+    GitAutograderOutput,
+    GitAutograderStatus,
+)
 
 MISSING_REPO = "You should have {repo} in your exercise folder. You might want to re-download the exercise."
 MISSING_COMMIT = "You should have made a separate commit!"
@@ -30,35 +32,26 @@ def run_command(command: List[str], verbose: bool) -> Optional[str]:
         return None
 
 
-def verify() -> GitAutograderOutput:
-    started_at = datetime.now(tz=pytz.UTC)
-
+def verify(exercise: GitAutograderExercise) -> GitAutograderOutput:
     username = run_command(["gh", "api", "user", "-q", ".login"], verbose=False)
     repo_name = f"{username}-gitmastery-push-over"
 
     if not os.path.isdir(repo_name):
-        return GitAutograderOutput(
-            status=GitAutograderStatus.UNSUCCESSFUL,
-            started_at=started_at,
-            completed_at=datetime.now(tz=pytz.UTC),
-            comments=[MISSING_REPO.format(repo=repo_name)],
-        )
+        raise exercise.wrong_answer([MISSING_REPO.format(repo=repo_name)])
 
-    repo = GitAutograderRepo("push-over", repo_name)
-
-    main_branch = repo.branches.branch("main")
+    main_branch = exercise.repo.branches.branch("main")
     if len(main_branch.commits) == 1:
-        raise repo.wrong_answer([MISSING_COMMIT])
+        raise exercise.wrong_answer([MISSING_COMMIT])
 
-    origin_remote = repo.remotes.remote("origin")
+    origin_remote = exercise.repo.remotes.remote("origin")
     origin_remote.remote.fetch()
-    remote_branch = repo.repo.refs["origin/main"]
-    remote_commits = list(repo.repo.iter_commits(remote_branch))
+    remote_branch = exercise.repo.repo.refs["origin/main"]
+    remote_commits = list(exercise.repo.repo.iter_commits(remote_branch))
 
     if len(remote_commits) == 1:
-        raise repo.wrong_answer([MISSING_COMMIT_REMOTE])
+        raise exercise.wrong_answer([MISSING_COMMIT_REMOTE])
 
-    return repo.to_output(
+    return exercise.to_output(
         ["Great work pushing changes to the remote!"],
-        status=GitAutograderStatus.SUCCESSFUL,
+        GitAutograderStatus.SUCCESSFUL,
     )
