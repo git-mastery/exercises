68b3c6067ef1232bed8da89340f617fe1fdabb4f
woojiahao <woojiahao@users.noreply.github.com>
2025-06-21 16:53:00 +0000

    deploy: d79b319883f2394bffe869cc8a4a08505015ddbc

--- remote_control/download.py
+++ remote_control/download.py
@@ -6,4 +6,6 @@ def setup(verbose: bool = False):
     print(os.getcwd())
     os.chdir("..")
     print(os.listdir())
+    print(os.getcwd())
     shutil.rmtree(f"{os.getcwd()}/deleted", ignore_errors=False)
+    assert not os.path.exists("deleted"), "Still exists after rmtree!"
