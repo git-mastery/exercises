28fa0687515156075fd2ad22830ef2c5deade771
Jiahao, Woo <woojiahao1234@gmail.com>
2025-06-21 23:45:44 +0800

    [amateur-detective] Update verify to use GitAutograderExercise instead

--- amateur_detective/verify.py
+++ amateur_detective/verify.py
@@ -1,20 +1,26 @@
-from git_autograder import GitAutograderOutput, GitAutograderRepo, GitAutograderStatus
+import os
+from git_autograder import (
+    GitAutograderExercise,
+    GitAutograderOutput,
+    GitAutograderStatus,
+)
 from git_autograder.answers.rules import HasExactValueRule, NotEmptyRule
 
 QUESTION_ONE = "Which file was added?"
 QUESTION_TWO = "Which file was edited?"
 
 
-def verify(repo: GitAutograderRepo) -> GitAutograderOutput:
+def verify(exercise: GitAutograderExercise) -> GitAutograderOutput:
+    print(os.listdir(exercise.repo.repo_path))
     (
-        repo.answers.add_validation(QUESTION_ONE, NotEmptyRule())
+        exercise.answers.add_validation(QUESTION_ONE, NotEmptyRule())
         .add_validation(QUESTION_ONE, HasExactValueRule("file77.txt"))
         .add_validation(QUESTION_TWO, NotEmptyRule())
         .add_validation(QUESTION_TWO, HasExactValueRule("file14.txt"))
         .validate()
     )
 
-    return repo.to_output(
+    return exercise.to_output(
         ["Congratulations on cracking the case! You found the hacker!"],
-        status=GitAutograderStatus.SUCCESSFUL,
+        GitAutograderStatus.SUCCESSFUL,
     )
