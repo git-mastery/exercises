a6910eaa6395c12a477ff7a090d4aeac0c9a3689
Jiahao, Woo <woojiahao1234@gmail.com>
2025-06-28 15:42:54 +0800

    [branch-bender] Add grader

--- branch_bender/verify.py
+++ branch_bender/verify.py
@@ -1,11 +1,62 @@
 from git_autograder import (
-    GitAutograderOutput,
     GitAutograderExercise,
+    GitAutograderInvalidStateException,
+    GitAutograderOutput,
     GitAutograderStatus,
+    GitAutograderWrongAnswerException,
 )
+
+UNCOMMITTED_CHANGES = "You still have uncommitted changes. Commit them first on the appropriate branch first!"
+NOT_ON_MAIN = (
+    "You aren't currently on the main branch. Checkout to that branch and try again!"
+)
+DETACHED_HEAD = "You should not be in a detached HEAD state! Use git checkout main to get back to main"
+MISSING_MERGES = "You are missing some merges"
+WRONG_MERGE_ORDER = "You should have merged {branch_name} first. The expected order is feature/login, feature/dashboard, feature/payments"
 
 
 def verify(exercise: GitAutograderExercise) -> GitAutograderOutput:
-    # INSERT YOUR GRADING CODE HERE
+    main_branch = exercise.repo.branches.branch("main")
+    if exercise.repo.repo.is_dirty():
+        raise exercise.wrong_answer([UNCOMMITTED_CHANGES])
 
-    return exercise.to_output([], GitAutograderStatus.SUCCESSFUL)
+    try:
+        if exercise.repo.repo.active_branch.name != "main":
+            raise exercise.wrong_answer([NOT_ON_MAIN])
+    except TypeError:
+        raise exercise.wrong_answer([DETACHED_HEAD])
+
+    try:
+        # Merge commits exhibit the behavior of having 2 parents (from/to)
+        merge_commits = [
+            commit for commit in main_branch.user_commits if len(commit.parents) > 1
+        ]
+        if len(merge_commits) < 3:
+            raise exercise.wrong_answer([MISSING_MERGES])
+
+        main_reflog = main_branch.reflog[::-1]
+        expected_order = ["feature/payments", "feature/dashboard", "feature/login"][
+            ::-1
+        ]
+        i = 0
+        for entry in main_reflog:
+            if not entry.action.startswith("merge") or i >= len(expected_order):
+                continue
+            merged_branch = entry.action[len("merge ") :]
+            if merged_branch != expected_order[i]:
+                raise exercise.wrong_answer(
+                    [WRONG_MERGE_ORDER.format(branch_name=expected_order[i])]
+                )
+            else:
+                i += 1
+
+        return exercise.to_output(
+            ["Great work with using git merge fix the bugs!"],
+            GitAutograderStatus.SUCCESSFUL,
+        )
+    except (GitAutograderWrongAnswerException, GitAutograderInvalidStateException):
+        raise
+    except Exception:
+        raise exercise.wrong_answer(["Something bad happened"])
+    finally:
+        main_branch.checkout()
