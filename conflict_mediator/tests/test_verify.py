1ba63bdea8186b4cd56d665ed0c90ab18628daf3
woojiahao <woojiahao@users.noreply.github.com>
2025-06-30 02:29:31 +0000

    deploy: b2802426750c1666ba579a8d1d4457bda43530e5

--- conflict_mediator/tests/test_verify.py
+++ conflict_mediator/tests/test_verify.py
@@ -1,12 +1,37 @@
-from git_autograder import GitAutograderTestLoader
+from git_autograder import GitAutograderStatus, GitAutograderTestLoader, assert_output
 
-from ..verify import verify
+from ..verify import (
+    MERGE_NOT_RESOLVED,
+    NOT_ON_MAIN,
+    RESET_MESSAGE,
+    UNCOMMITTED_CHANGES,
+    verify,
+)
 
 REPOSITORY_NAME = "conflict-mediator"
 
 loader = GitAutograderTestLoader(__file__, REPOSITORY_NAME, verify)
 
 
-def test():
-    with loader.load("specs/base.yml", "start"):
-        pass
+def test_base():
+    with loader.load("specs/base.yml", "start") as output:
+        assert_output(output, GitAutograderStatus.SUCCESSFUL)
+
+
+def test_no_fix():
+    with loader.load("specs/no_fix.yml", "start") as output:
+        assert_output(
+            output,
+            GitAutograderStatus.UNSUCCESSFUL,
+            [MERGE_NOT_RESOLVED, RESET_MESSAGE],
+        )
+
+
+def test_uncommitted():
+    with loader.load("specs/uncommitted.yml") as output:
+        assert_output(output, GitAutograderStatus.UNSUCCESSFUL, [UNCOMMITTED_CHANGES])
+
+
+def test_not_main():
+    with loader.load("specs/not_main.yml") as output:
+        assert_output(output, GitAutograderStatus.UNSUCCESSFUL, [NOT_ON_MAIN])
--- conflict_mediator/verify.py
+++ conflict_mediator/verify.py
@@ -1,11 +1,30 @@
 from git_autograder import (
-    GitAutograderOutput,
     GitAutograderExercise,
+    GitAutograderOutput,
     GitAutograderStatus,
 )
 
+UNCOMMITTED_CHANGES = "You still have uncommitted changes. Commit them first on the appropriate branch first!"
+NOT_ON_MAIN = (
+    "You aren't currently on the main branch. Checkout to that branch and try again!"
+)
+DETACHED_HEAD = "You should not be in a detached HEAD state! Run git checkout main to get back to main"
+MERGE_NOT_RESOLVED = "You should resolve the merge by setting the print to be 'Hello Everyone and World!'"
+RESET_MESSAGE = 'Reset the repository using "gitmastery progress reset" and start again'
+
 
 def verify(exercise: GitAutograderExercise) -> GitAutograderOutput:
-    # INSERT YOUR GRADING CODE HERE
+    if exercise.repo.repo.is_dirty():
+        raise exercise.wrong_answer([UNCOMMITTED_CHANGES])
+
+    try:
+        if exercise.repo.repo.active_branch.name != "main":
+            raise exercise.wrong_answer([NOT_ON_MAIN])
+    except TypeError:
+        raise exercise.wrong_answer([DETACHED_HEAD])
+
+    with exercise.repo.files.file("script.py") as script_file:
+        if script_file.read().strip() != 'print("Hello Everyone and World!")':
+            raise exercise.wrong_answer([MERGE_NOT_RESOLVED, RESET_MESSAGE])
 
     return exercise.to_output([], GitAutograderStatus.SUCCESSFUL)
