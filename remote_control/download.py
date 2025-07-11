cfcb026cbdb2361208dc94e3c82766a7cb1e2673
Jiahao, Woo <woojiahao1234@gmail.com>
2025-07-11 15:41:58 +0800

    [remote-control] Fix exercise folder missing

--- remote_control/download.py
+++ remote_control/download.py
@@ -2,8 +2,4 @@ import os
 import shutil
 
 
-def setup(verbose: bool = False):
-    # TODO: Resolve this bug where the assert doesn't happen but the folder still exists
-    os.chdir("..")
-    shutil.rmtree(f"{os.getcwd()}/deleted", ignore_errors=False)
-    assert not os.path.exists("deleted"), "Still exists after rmtree!"
+def setup(verbose: bool = False): ...
