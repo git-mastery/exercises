f0748379c8709a5464e8cbfc270590ee355eb0c3
Jiahao, Woo <woojiahao1234@gmail.com>
2025-07-14 14:48:58 +0800

    [staging-intervention] Add verification script

--- staging_intervention/verify.py
+++ staging_intervention/verify.py
@@ -1,11 +1,40 @@
+from dataclasses import MISSING
 from git_autograder import (
     GitAutograderOutput,
     GitAutograderExercise,
     GitAutograderStatus,
 )
 
+EXPECTED_UNTRACKED = {
+    "josh.txt",
+    "adam.txt",
+    "mary.txt",
+}
+EXTRA_FILES_UNSTAGED = (
+    "You should only be unstaging the attendance files for Josh, Adam, and Mary."
+)
+MISSING_FILES_UNSTAGED = (
+    "You need to unstage all three attendance files for Josh, Adam, and Mary."
+)
+
 
 def verify(exercise: GitAutograderExercise) -> GitAutograderOutput:
-    # INSERT YOUR GRADING CODE HERE
+    untracked_files = set(exercise.repo.repo.untracked_files)
+    comments = []
 
-    return exercise.to_output([], GitAutograderStatus.SUCCESSFUL)
+    extra_files_unstaged = untracked_files.difference(EXPECTED_UNTRACKED)
+    missing_files_unstaged = EXPECTED_UNTRACKED.difference(untracked_files)
+
+    if extra_files_unstaged:
+        comments.append(EXTRA_FILES_UNSTAGED)
+
+    if missing_files_unstaged:
+        comments.append(MISSING_FILES_UNSTAGED)
+
+    if comments:
+        raise exercise.wrong_answer(comments)
+
+    return exercise.to_output(
+        ["Great work! You have successfully fixed the attendance system!"],
+        GitAutograderStatus.SUCCESSFUL,
+    )
