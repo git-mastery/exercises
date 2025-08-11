2114788a6f0f130b731d28732c0803cb541f9d8d
Jiahao, Woo <woojiahao1234@gmail.com>
2025-08-11 14:02:32 +0800

    [branch-rename] Add verification

--- branch_rename/verify.py
+++ branch_rename/verify.py
@@ -1,11 +1,45 @@
+from git import Repo
 from git_autograder import (
-    GitAutograderOutput,
     GitAutograderExercise,
+    GitAutograderOutput,
     GitAutograderStatus,
 )
 
+LOGIN_STILL_EXISTS = (
+    "Branch 'login' still exists! Remember to rename it to 'feature/login'"
+)
+FEATURE_LOGIN_MISSING = "Branch 'feature/login' is missing, did you correctly rename the branch 'login' to 'feature/login'?"
+NO_RENAME_EVIDENCE_FEATURE_LOGIN = "Branch 'login' was not renamed to 'feature/login'!"
+
+
+def branch_has_rename_evidence(
+    exercise: GitAutograderExercise, new_branch: str, old_branch: str
+) -> bool:
+    branch = exercise.repo.branches.branch_or_none(new_branch)
+    if branch is None:
+        return False
+
+    expected = f"renamed refs/heads/{old_branch} to refs/heads/{new_branch}"
+    for entry in branch.reflog:
+        if entry.message == expected:
+            return True
+    return False
+
 
 def verify(exercise: GitAutograderExercise) -> GitAutograderOutput:
-    # INSERT YOUR GRADING CODE HERE
+    repo: Repo = exercise.repo.repo
 
-    return exercise.to_output([], GitAutograderStatus.SUCCESSFUL)
+    local_branches = [h.name for h in repo.heads]
+    if "login" in local_branches:
+        raise exercise.wrong_answer([LOGIN_STILL_EXISTS])
+
+    if "feature/login" not in local_branches:
+        raise exercise.wrong_answer([FEATURE_LOGIN_MISSING])
+
+    if not branch_has_rename_evidence(exercise, "feature/login", "login"):
+        raise exercise.wrong_answer([NO_RENAME_EVIDENCE_FEATURE_LOGIN])
+
+    return exercise.to_output(
+        ["Great work with renaming the branches on your local repository!"],
+        GitAutograderStatus.SUCCESSFUL,
+    )
