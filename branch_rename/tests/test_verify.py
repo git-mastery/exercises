c2460c32e711dd247354ab8d49876ff6f66d52ef
Jiahao, Woo <woojiahao1234@gmail.com>
2025-08-11 14:02:36 +0800

    [branch-rename] Add unit tests

--- branch_rename/tests/test_verify.py
+++ branch_rename/tests/test_verify.py
@@ -1,6 +1,12 @@
-from git_autograder import GitAutograderTestLoader
+from git_autograder import GitAutograderTestLoader, assert_output
+from git_autograder.status import GitAutograderStatus
 
-from ..verify import verify
+from ..verify import (
+    FEATURE_LOGIN_MISSING,
+    LOGIN_STILL_EXISTS,
+    NO_RENAME_EVIDENCE_FEATURE_LOGIN,
+    verify,
+)
 
 REPOSITORY_NAME = "branch-rename"
 
@@ -8,5 +14,22 @@ loader = GitAutograderTestLoader(__file__, REPOSITORY_NAME, verify)
 
 
 def test_base():
-    with loader.load("specs/base.yml", "start"):
-        pass
+    with loader.load("specs/base.yml") as output:
+        assert_output(output, GitAutograderStatus.SUCCESSFUL)
+
+
+def test_new_feature_login_branch():
+    with loader.load("specs/new_feature_login_branch.yml") as output:
+        assert_output(output, GitAutograderStatus.UNSUCCESSFUL, [LOGIN_STILL_EXISTS])
+
+
+def test_rename_login_wrong():
+    with loader.load("specs/rename_login_wrong.yml") as output:
+        assert_output(output, GitAutograderStatus.UNSUCCESSFUL, [FEATURE_LOGIN_MISSING])
+
+
+def test_not_rename():
+    with loader.load("specs/not_rename.yml") as output:
+        assert_output(
+            output, GitAutograderStatus.UNSUCCESSFUL, [NO_RENAME_EVIDENCE_FEATURE_LOGIN]
+        )
