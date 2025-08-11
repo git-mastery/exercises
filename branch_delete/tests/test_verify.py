fb3588feb49b4fed1a3b868590361a24688933ce
Jiahao, Woo <woojiahao1234@gmail.com>
2025-08-11 13:26:36 +0800

    [branch-delete] Add unit tests

--- branch_delete/tests/test_verify.py
+++ branch_delete/tests/test_verify.py
@@ -1,6 +1,12 @@
-from git_autograder import GitAutograderTestLoader
+from git_autograder import GitAutograderTestLoader, assert_output
+from git_autograder.status import GitAutograderStatus
 
-from ..verify import verify
+from ..verify import (
+    FIX_SCROLLING_BUG_EXISTS,
+    IMPROVE_LOADING_EXISTS,
+    IMPROVE_LOADING_MERGED,
+    verify,
+)
 
 REPOSITORY_NAME = "branch-delete"
 
@@ -8,5 +14,26 @@ loader = GitAutograderTestLoader(__file__, REPOSITORY_NAME, verify)
 
 
 def test_base():
-    with loader.load("specs/base.yml", "start"):
-        pass
+    with loader.load("specs/base.yml") as output:
+        assert_output(output, GitAutograderStatus.SUCCESSFUL)
+
+
+def test_fix_scrolling_bug_not_deleted():
+    with loader.load("specs/fix_scrolling_bug_not_deleted.yml") as output:
+        assert_output(
+            output, GitAutograderStatus.UNSUCCESSFUL, [FIX_SCROLLING_BUG_EXISTS]
+        )
+
+
+def test_improve_loading_not_deleted():
+    with loader.load("specs/improve_loading_not_deleted.yml") as output:
+        assert_output(
+            output, GitAutograderStatus.UNSUCCESSFUL, [IMPROVE_LOADING_EXISTS]
+        )
+
+
+def test_improve_loading_merged():
+    with loader.load("specs/improve_loading_merged.yml") as output:
+        assert_output(
+            output, GitAutograderStatus.UNSUCCESSFUL, [IMPROVE_LOADING_MERGED]
+        )
