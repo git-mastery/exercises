6ae4f1368c44b33840ec48a1bb37ef253a647bc7
Jiahao, Woo <woojiahao1234@gmail.com>
2025-08-11 12:37:38 +0800

    [branch-delete] Add verification

--- branch_delete/verify.py
+++ branch_delete/verify.py
@@ -1,11 +1,34 @@
 from git_autograder import (
-    GitAutograderOutput,
     GitAutograderExercise,
+    GitAutograderOutput,
     GitAutograderStatus,
 )
 
+FIX_SCROLLING_BUG_EXISTS = (
+    "Branch 'fix-scrolling-bug' still exists! Remember to delete it"
+)
+IMPROVE_LOADING_EXISTS = "Branch 'improve-loading' still exists! Remember to delete it"
+IMPROVE_LOADING_MERGED = (
+    "Branch 'improve-loading' was merged into 'main', but it shouldn't be"
+)
+PROGRESS_RESET = "Reset your progress using 'gitmastery progress reset' to try again!"
+
 
 def verify(exercise: GitAutograderExercise) -> GitAutograderOutput:
-    # INSERT YOUR GRADING CODE HERE
+    if exercise.repo.branches.has_branch("fix-scrolling-bug"):
+        raise exercise.wrong_answer([FIX_SCROLLING_BUG_EXISTS])
 
-    return exercise.to_output([], GitAutograderStatus.SUCCESSFUL)
+    if exercise.repo.branches.has_branch("improve-loading"):
+        raise exercise.wrong_answer([IMPROVE_LOADING_EXISTS])
+
+    main_reflog = exercise.repo.branches.branch("main").reflog
+    merge_logs = [entry for entry in main_reflog if entry.action.startswith("merge")]
+    merge_order = [entry.action[len("merge ") :] for entry in merge_logs][::-1]
+    for merge in merge_order:
+        if merge == "improve-loading":
+            raise exercise.wrong_answer([IMPROVE_LOADING_MERGED])
+
+    return exercise.to_output(
+        ["Great job using git branch to delete both merged and unmerged branches!"],
+        GitAutograderStatus.SUCCESSFUL,
+    )
