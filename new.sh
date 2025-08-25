bdf44c2b8fdab11da5a80962f544d1cee505569d
Jiahao, Woo <woojiahao1234@gmail.com>
2025-08-25 08:26:45 +0100

    [tooling] Setup script for hands-on scaffolding

--- new.sh
+++ new.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-python new-exercise.py
+echo "Do you want to create a hands-on or an exercise? (hands-on/exercise, h/e)"
+read choice
+
+case "$choice" in
+"hands-on" | "h" | "H")
+  python new-hands-on.py
+  ;;
+"exercise" | "e" | "E")
+  python new-exercise.py
+  ;;
+*)
+  echo "Invalid choice. Please enter 'hands-on' (h) or 'exercise' (e)."
+  ;;
+esac
