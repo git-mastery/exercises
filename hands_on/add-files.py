8088e52c70cb35ccd8d5f7ecae2474cbfb209660
Jiahao, Woo <woojiahao1234@gmail.com>
2025-09-01 21:32:37 +0100

    [hp-add-files] Add hands-on scaffolding

---
+++ hands_on/add-files.py
@@ -0,0 +1,27 @@
+import subprocess
+from sys import exit
+from typing import List, Optional
+
+__requires_git__ = True
+__requires_github__ = False
+
+
+def run_command(command: List[str], verbose: bool) -> Optional[str]:
+    try:
+        result = subprocess.run(
+            command,
+            capture_output=True,
+            text=True,
+            check=True,
+        )
+        if verbose:
+            print(result.stdout)
+        return result.stdout
+    except subprocess.CalledProcessError as e:
+        if verbose:
+            print(e.stderr)
+        exit(1)
+
+
+def download(verbose: bool):
+    pass
