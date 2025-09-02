7ebcd0b73eff114b6e32dd8bce8380f45b38eb43
woojiahao <woojiahao@users.noreply.github.com>
2025-09-02 06:55:01 +0000

    deploy: 8e619a5b3370cf6c7b50e36e91fd52c56b52a14d

--- hands_on/add_files.py
+++ hands_on/add_files.py
@@ -1,27 +1,9 @@
 import os
-import subprocess
-from sys import exit
-from typing import List, Optional
+
+from utils.cmd import run_command
 
 __requires_git__ = True
 __requires_github__ = False
-
-
-def run_command(command: List[str], verbose: bool) -> Optional[str]:
-    try:
-        result = subprocess.run(
-            command,
-            capture_output=True,
-            text=True,
-            check=True,
-        )
-        if verbose:
-            print(result.stdout)
-        return result.stdout
-    except subprocess.CalledProcessError as e:
-        if verbose:
-            print(e.stderr)
-        exit(1)
 
 
 def download(verbose: bool):
