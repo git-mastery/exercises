76f6eba97f297359ab798c364388610846ba7645
Jiahao, Woo <woojiahao1234@gmail.com>
2025-09-10 21:05:15 +0100

    [log-and-order] Setup verification to pick up on dynamic SHA

--- log_and_order/tests/test_verify.py
+++ log_and_order/tests/test_verify.py
@@ -12,14 +12,16 @@ loader = GitAutograderTestLoader(__file__, REPOSITORY_NAME, verify)
 def test_base():
     with (
         patch("log_and_order.verify.get_head_sha", return_value="a" * 15),
-        patch("log_and_order.verify.get_head_message", return_value="Hello world"),
+        patch(
+            "log_and_order.verify.get_target_commit_message", return_value="Hello world"
+        ),
         patch("log_and_order.verify.get_target_commit_sha", return_value="b" * 15),
         loader.load(
             "specs/base.yml",
             "start",
             mock_answers={
                 QUESTION_ONE: "a" * 7,
-                QUESTION_TWO: "Hello world",
+                QUESTION_TWO.format(SHA="hi"): "Hello world",
                 QUESTION_THREE: "b" * 7,
             },
         ) as output,
@@ -30,14 +32,16 @@ def test_base():
 def test_wrong_head_sha():
     with (
         patch("log_and_order.verify.get_head_sha", return_value="a" * 15),
-        patch("log_and_order.verify.get_head_message", return_value="Hello world"),
+        patch(
+            "log_and_order.verify.get_target_commit_message", return_value="Hello world"
+        ),
         patch("log_and_order.verify.get_target_commit_sha", return_value="b" * 15),
         loader.load(
             "specs/base.yml",
             "start",
             mock_answers={
                 QUESTION_ONE: "b" * 7,
-                QUESTION_TWO: "Hello world",
+                QUESTION_TWO.format(SHA="hi"): "Hello world",
                 QUESTION_THREE: "b" * 7,
             },
         ) as output,
@@ -52,14 +56,16 @@ def test_wrong_head_sha():
 def test_wrong_head_message():
     with (
         patch("log_and_order.verify.get_head_sha", return_value="a" * 15),
-        patch("log_and_order.verify.get_head_message", return_value="Hello world"),
+        patch(
+            "log_and_order.verify.get_target_commit_message", return_value="Hello world"
+        ),
         patch("log_and_order.verify.get_target_commit_sha", return_value="b" * 15),
         loader.load(
             "specs/base.yml",
             "start",
             mock_answers={
                 QUESTION_ONE: "a" * 7,
-                QUESTION_TWO: "Bye world",
+                QUESTION_TWO.format(SHA="hi"): "Bye world",
                 QUESTION_THREE: "b" * 7,
             },
         ) as output,
@@ -67,21 +73,27 @@ def test_wrong_head_message():
         assert_output(
             output,
             GitAutograderStatus.UNSUCCESSFUL,
-            [HasExactValueRule.NOT_EXACT.format(question=QUESTION_TWO)],
+            [
+                HasExactValueRule.NOT_EXACT.format(
+                    question=QUESTION_TWO.format(SHA="hi")
+                )
+            ],
         )
 
 
 def test_wrong_target_sha():
     with (
         patch("log_and_order.verify.get_head_sha", return_value="a" * 15),
-        patch("log_and_order.verify.get_head_message", return_value="Hello world"),
+        patch(
+            "log_and_order.verify.get_target_commit_message", return_value="Hello world"
+        ),
         patch("log_and_order.verify.get_target_commit_sha", return_value="b" * 15),
         loader.load(
             "specs/base.yml",
             "start",
             mock_answers={
                 QUESTION_ONE: "a" * 7,
-                QUESTION_TWO: "Hello world",
+                QUESTION_TWO.format(SHA="hi"): "Hello world",
                 QUESTION_THREE: "a" * 7,
             },
         ) as output,
