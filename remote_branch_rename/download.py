882845311a746ee830b2cbe646894a0f048cee50
Jiahao, Woo <woojiahao1234@gmail.com>
2025-09-13 15:12:56 +0100

    [remote-branch-rename] Use new utils

--- remote_branch_rename/download.py
+++ remote_branch_rename/download.py
@@ -1,37 +1,15 @@
-import subprocess
-from sys import exit
-from typing import List, Optional
-
-
-def run_command(command: List[str], verbose: bool) -> Optional[str]:
-    try:
-        result = subprocess.run(
-            command,
-            capture_output=True,
-            text=True,
-            check=True,
-        )
-        if verbose:
-            print(result.stdout)
-        return result.stdout
-    except subprocess.CalledProcessError as e:
-        if verbose:
-            print(e.stderr)
-        exit(1)
+from exercise_utils.git import checkout, empty_commit, push
 
 
 def setup(verbose: bool = False):
-    run_command(["git", "checkout", "-b", "try-quick-fix"], verbose)
-    run_command(
-        ["git", "commit", "--allow-empty", "-m", "Fixed scrolling issue"], verbose
-    )
+    checkout("try-quick-fix", True, verbose)
+    empty_commit("Fixed scrolling issue", verbose)
 
-    run_command(["git", "checkout", "main"], verbose)
+    checkout("main", False, verbose)
 
-    run_command(["git", "checkout", "-b", "improve-loadding"], verbose)
-    run_command(
-        ["git", "commit", "--allow-empty", "-m", "Improved loading of page"], verbose
-    )
-    run_command(["git", "push", "origin", "improve-loadding"], verbose)
+    checkout("improve-loadding", True, verbose)
+    empty_commit("Improved loading of page", verbose)
 
-    run_command(["git", "checkout", "main"], verbose)
+    push("origin", "improve-loadding", verbose)
+
+    checkout("main", False, verbose)
