5f401d93728b143d868aa7f19bb58d6b69fb950a
Jiahao, Woo <woojiahao1234@gmail.com>
2025-09-13 15:11:55 +0100

    [branch-rename] Use new utils

--- branch_rename/download.py
+++ branch_rename/download.py
@@ -1,41 +1,16 @@
-import subprocess
-from sys import exit
-from typing import List, Optional
-
-
-def run_command(command: List[str], verbose: bool) -> Optional[str]:
-    try:
-        result = subprocess.run(
-            command,
-            capture_output=True,
-            text=True,
-            check=True,
-        )
-        if verbose:
-            print(result.stdout)
-        return result.stdout
-    except subprocess.CalledProcessError as e:
-        if verbose:
-            print(e.stderr)
-        exit(1)
+from exercise_utils.git import checkout, empty_commit
 
 
 def setup(verbose: bool = False):
-    run_command(["git", "commit", "--allow-empty", "-m", "Initialize project"], verbose)
-    run_command(
-        ["git", "commit", "--allow-empty", "-m", "Add React boilerplate"], verbose
-    )
+    empty_commit("Initialize project", verbose)
+    empty_commit("Add React boilerplate", verbose)
 
-    run_command(["git", "checkout", "-b", "login"], verbose)
-    run_command(
-        ["git", "commit", "--allow-empty", "-m", "Implement login feature"], verbose
-    )
+    checkout("login", True, verbose)
+    empty_commit("Implement login feature", verbose)
 
-    run_command(["git", "checkout", "main"], verbose)
-    run_command(["git", "commit", "--allow-empty", "-m", "Create homepage"], verbose)
+    checkout("main", False, verbose)
+    empty_commit("Create homepage", verbose)
 
-    run_command(["git", "checkout", "login"], verbose)
-    run_command(
-        ["git", "commit", "--allow-empty", "-m", "Fix login password bug"], verbose
-    )
-    run_command(["git", "checkout", "main"], verbose)
+    checkout("login", False, verbose)
+    empty_commit("Fix login password bug", verbose)
+    checkout("main", False, verbose)
