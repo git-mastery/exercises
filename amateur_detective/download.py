ebb604696c4c735d63c0e9e705d9c8add51305ac
Jiahao, Woo <woojiahao1234@gmail.com>
2025-09-13 15:06:20 +0100

    [amateur-detective] Use new utils

--- amateur_detective/download.py
+++ amateur_detective/download.py
@@ -1,46 +1,25 @@
-import subprocess
-from sys import exit
-from typing import List, Optional
+import os
+import stat
 
-
-def run_command(command: List[str], verbose: bool) -> Optional[str]:
-    try:
-        result = subprocess.run(
-            command,
-            capture_output=True,
-            text=True,
-            check=True,
-        )
-        if verbose:
-            print(result.stdout)
-        return result.stdout
-    except subprocess.CalledProcessError as e:
-        if verbose:
-            print(e.stderr)
-        exit(1)
+from branch_bender.download import create_or_update_file
+from exercise_utils.file import append_to_file
+from exercise_utils.git import add, commit
+from exercise_utils.gitmastery import create_start_tag
 
 
 def setup(verbose: bool = False):
     for i in range(1, 101):
         if i == 77:
             continue
-        open(f"file{i}.txt", "a").close()
+        create_or_update_file(f"file{i}.txt")
 
-    run_command(
-        ["git", "add"] + [f"file{i}.txt" for i in range(1, 101) if i != 77], verbose
-    )
-    run_command(["git", "commit", "-m", "Change 1"], verbose)
+    add([f"file{i}.txt" for i in range(1, 101) if i != 77], verbose)
+    commit("Change 1", verbose)
 
-    with open("file14.txt", "a") as file:
-        file.write("This is a change")
+    append_to_file("file14.txt", "This is a change")
 
-    open("file77.txt", "a").close()
-    run_command(["chmod", "-w"] + [f"file{i}.txt" for i in range(1, 101)], verbose)
+    create_or_update_file("file77.txt")
+    for i in range(1, 101):
+        os.chmod(f"file{i}.txt", stat.S_IREAD)
 
-    commits_str = run_command(
-        ["git", "log", "--reverse", "--pretty=format:%h"], verbose
-    )
-    assert commits_str is not None
-    first_commit = commits_str.split("\n")[0]
-    tag_name = f"git-mastery-start-{first_commit}"
-    run_command(["git", "tag", tag_name], verbose)
+    create_start_tag(verbose)
--- conflict_mediator/download.py
+++ conflict_mediator/download.py
@@ -1,78 +1,23 @@
-import os
-import pathlib
-import subprocess
-import textwrap
-from sys import exit
-from typing import List, Optional
+from exercise_utils.file import create_or_update_file
+from exercise_utils.git import add, checkout, commit, merge
+from exercise_utils.gitmastery import create_start_tag
 
 __resources__ = {"script.py": "script.py"}
 
 
-def run_command(command: List[str], verbose: bool) -> Optional[str]:
-    try:
-        result = subprocess.run(
-            command,
-            capture_output=True,
-            text=True,
-            check=True,
-        )
-        if verbose:
-            print(result.stdout)
-        return result.stdout
-    except subprocess.CalledProcessError as e:
-        if verbose:
-            print(e.stderr)
-        exit(1)
-
-
-def run_command_no_exit(command: List[str], verbose: bool) -> Optional[str]:
-    try:
-        result = subprocess.run(
-            command,
-            capture_output=True,
-            text=True,
-            check=True,
-        )
-        if verbose:
-            print(result.stdout)
-        return result.stdout
-    except subprocess.CalledProcessError as e:
-        if verbose:
-            print(e.stderr)
-        return None
-
-
-def create_or_update_file(
-    filepath: str | pathlib.Path, contents: Optional[str] = None
-) -> None:
-    if os.path.dirname(filepath) != "":
-        os.makedirs(os.path.dirname(filepath), exist_ok=True)
-    if contents is None:
-        open(filepath, "a").close()
-    else:
-        with open(filepath, "w") as file:
-            file.write(textwrap.dedent(contents).lstrip())
-
-
 def setup(verbose: bool = False):
-    commits_str = run_command(
-        ["git", "log", "--reverse", "--pretty=format:%h"], verbose
-    )
-    assert commits_str is not None
-    first_commit = commits_str.split("\n")[0]
-    tag_name = f"git-mastery-start-{first_commit}"
-    run_command(["git", "tag", tag_name], verbose)
+    create_start_tag(verbose)
 
-    run_command(["git", "checkout", "-b", "john"], verbose)
+    checkout("john", True, verbose)
     create_or_update_file("script.py", "print('Hello World!')")
-    run_command(["git", "add", "script.py"], verbose)
-    run_command(["git", "commit", "-m", "Hello world"], verbose)
+    add(["script.py"], verbose)
+    commit("Hello world", verbose)
 
-    run_command(["git", "checkout", "main"], verbose)
-    run_command(["git", "checkout", "-b", "josh"], verbose)
+    checkout("main", False, verbose)
+    checkout("josh", True, verbose)
     create_or_update_file("script.py", "print('Hello Everyone!')")
-    run_command(["git", "add", "script.py"], verbose)
-    run_command(["git", "commit", "-m", "Hello everyone"], verbose)
+    add(["script.py"], verbose)
+    commit("Hello everyone", verbose)
 
-    run_command(["git", "checkout", "main"], verbose)
-    run_command(["git", "merge", "john", "--no-edit"], verbose)
+    checkout("main", False, verbose)
+    merge("john", verbose)
