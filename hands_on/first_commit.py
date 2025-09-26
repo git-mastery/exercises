f06bcaa9815db714cf03927c084ba542982db74b
SAN-MUYUN <e1121291@u.nus.edu>
2025-09-26 22:32:48 +0800

    improve code quality

--- hands_on/first_commit.py
+++ hands_on/first_commit.py
@@ -1,7 +1,7 @@
 import os
 from exercise_utils.cli import run_command
 from exercise_utils.file import create_or_update_file
-from exercise_utils.git import add
+from exercise_utils.git import add, init
 
 __requires_git__ = True
 __requires_github__ = False
@@ -10,7 +10,7 @@ __requires_github__ = False
 def download(verbose: bool):
     os.makedirs("things")
     os.chdir("things")
-    run_command(["git", "init", "--initial-branch=main"], verbose)
+    init(verbose)
     create_or_update_file(
         "fruits.txt",
         """
