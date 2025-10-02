1b0db27e87da6b58136f68e3570672e3ea4aec4b
Jiahao, Woo <woojiahao1234@gmail.com>
2025-10-02 22:25:39 +0100

    [mix-messy-docs] Add simple tests

--- mix_messy_docs/tests/test_verify.py
+++ mix_messy_docs/tests/test_verify.py
@@ -1,6 +1,7 @@
-from git_autograder import GitAutograderTestLoader
+from git_autograder import GitAutograderStatus, GitAutograderTestLoader
+from git_autograder.test_utils import assert_output
 
-from ..verify import verify
+from ..verify import WRONG_BRANCH_POINT, verify
 
 REPOSITORY_NAME = "mix-messy-docs"
 
@@ -10,3 +11,13 @@ loader = GitAutograderTestLoader(__file__, REPOSITORY_NAME, verify)
 def test_base():
     with loader.load("specs/base.yml", "start"):
         pass
+
+
+def test_wrong_branch_point():
+    with loader.load("specs/wrong_branch_point.yml") as output:
+        assert_output(output, GitAutograderStatus.UNSUCCESSFUL, [WRONG_BRANCH_POINT])
+
+
+def test_right_order():
+    with loader.load("specs/right_order.yml") as output:
+        assert_output(output, GitAutograderStatus.SUCCESSFUL)
