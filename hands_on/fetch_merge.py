0e351d2f63ac6e1e084a41bf286937096132b6f0
LeeZeHao <zehaolee2004@gmail.com>
2025-10-29 18:08:53 +0800

    Add download for hp-fetch-merge

--- hands_on/fetch_merge.py
+++ hands_on/fetch_merge.py
@@ -1,3 +1,5 @@
+import os
+
 from exercise_utils.cli import run_command
 from exercise_utils.gitmastery import create_start_tag
 
@@ -6,4 +8,11 @@ __requires_github__ = True
 
 
 def download(verbose: bool):
+    os.makedirs("samplerepo-finances")
+    # Clone the samplerepo-finances repo
+    run_command(["git", "clone", "https://github.com/git-mastery/samplerepo-finances.git"], verbose) 
+    os.chdir("samplerepo-finances")
+    # Change the remote origin to point to samplerepo-finances-2
+    run_command(["git", "remote", "set-url", "origin", "https://github.com/git-mastery/samplerepo-finances-2.git"], verbose) 
+
     pass
