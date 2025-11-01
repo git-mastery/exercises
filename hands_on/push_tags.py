598a3b8f13f4ed24c1d43bbdd12ad9ab9db26d80
dingZvel <ding_zhe@u.nus.edu>
2025-11-01 18:56:30 +0800

    Fix the cloned repo directory name to REPO_NAME

--- hands_on/push_tags.py
+++ hands_on/push_tags.py
@@ -10,15 +10,10 @@ REPO_NAME = "samplerepo-preferences"
 
 def download(verbose: bool):
 
-    run_command(["gh", "repo", "fork", f"git-mastery/{REPO_NAME}", "--clone"],
+    run_command(["gh", "repo", "fork", f"git-mastery/{REPO_NAME}", REPO_NAME, "--clone"],
                 verbose)
 
-    if os.path.isdir(REPO_NAME):
-        os.chdir(REPO_NAME)
-    else:
-        # If user already has an repo with the same name, the gh command will
-        # atomatically create the fork with the name appended with "-1"
-        os.chdir(f"{REPO_NAME}-1")
+    os.chdir(REPO_NAME)
 
     tag_with_options("v1.0", ["HEAD~1"], verbose)
     annotated_tag_with_options("v0.9", ["HEAD~2", "-m", "First beta release"], verbose)
