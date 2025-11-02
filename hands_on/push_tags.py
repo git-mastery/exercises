5dc73ff3b1bb4e9c142a4db6076c5afd9e60dd77
dingZvel <ding_zhe@u.nus.edu>
2025-11-03 00:23:52 +0800

    Notify user when fork name is changed

--- hands_on/push_tags.py
+++ hands_on/push_tags.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 from exercise_utils.cli import run_command
 from exercise_utils.git import tag_with_options, annotated_tag_with_options
 
@@ -10,8 +11,24 @@ REPO_NAME = "samplerepo-preferences"
 
 def download(verbose: bool):
 
-    run_command(["gh", "repo", "fork", f"git-mastery/{REPO_NAME}", REPO_NAME, "--clone"],
-                verbose)
+    # Cannot use run_command function because returned object should not be None.
+    username = subprocess.run(
+        ["gh", "api", "user", "-q", ".login"],
+        capture_output=True,
+        text=True,
+    ).stdout.strip()
+
+    # Cannot use run_command function because return code is needed.
+    check_repo = subprocess.run(
+        ["gh", "repo", "view", f"{username}/{REPO_NAME}"],
+        capture_output=True,
+        text=True,
+    )
+
+    if check_repo.returncode == 0:
+        print(f"{username}/{REPO_NAME} already exists, the fork repo will be named as {username}/{REPO_NAME}-1")
+
+    run_command(["gh", "repo", "fork", f"git-mastery/{REPO_NAME}", REPO_NAME, "--clone"], verbose)
 
     os.chdir(REPO_NAME)
 
