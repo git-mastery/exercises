0d06001297ebe7055486ef9f434dbc5dedba57a2
izruff <rafaelky.dev@gmail.com>
2025-11-02 15:54:17 +0800

    Implement hp-add-tags download workflow

--- hands_on/add_tags.py
+++ hands_on/add_tags.py
@@ -1,9 +1,11 @@
 from exercise_utils.cli import run_command
-from exercise_utils.gitmastery import create_start_tag
 
 __requires_git__ = True
 __requires_github__ = True
 
+WORKING_REPO = "samplerepo-preferences"
+
 
 def download(verbose: bool):
-    pass
+    run_command(["gh", "repo", "fork", f"git-mastery/{WORKING_REPO}",
+                 "--clone"], verbose)
