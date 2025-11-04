13c71b44e2b919237b6efc91e8c3ef838be24533
YuLetian <yu.letian66@gmail.com>
2025-11-04 21:01:51 +0800

    Remove deprecated import

--- hands_on/diff_changes.py
+++ hands_on/diff_changes.py
@@ -1,9 +1,7 @@
 import os
 
-from exercise_utils.cli import run_command
-from exercise_utils.gitmastery import create_start_tag
-from exercise_utils.git import add, init, commit, tag
 from exercise_utils.file import create_or_update_file, append_to_file
+from exercise_utils.git import add, init, commit, tag
 
 __requires_git__ = True
 __requires_github__ = False
