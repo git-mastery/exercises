9eccc4d84ff64c263bb38326ddaf5b3519322892
lavanyagarg112 <lavanyagarg@u.nus.edu>
2025-11-08 19:50:39 +0800

    add unittest for invalid cases

--- branch_compare/tests/test_verify.py
+++ branch_compare/tests/test_verify.py
@@ -1,5 +1,6 @@
 from git_autograder import GitAutograderStatus, GitAutograderTestLoader, assert_output
 from unittest.mock import patch
+from git_autograder.answers.rules.has_exact_value_rule import HasExactValueRule
 
 from ..verify import verify, QUESTION_ONE, QUESTION_TWO
 
@@ -22,3 +23,41 @@ def test_base():
         ) as output,
     ):
         assert_output(output, GitAutograderStatus.SUCCESSFUL)
+
+def test_wrong_stream1_diff():
+    with (
+        patch("branch_compare.verify.get_stream1_diff", return_value="99999"),
+        patch("branch_compare.verify.get_stream2_diff", return_value="98765"),
+        loader.load(
+            "specs/base.yml",
+            "start",
+            mock_answers={
+                QUESTION_ONE: "12345",
+                QUESTION_TWO: "98765",
+            }
+        ) as output,
+    ):
+        assert_output(
+            output,
+            GitAutograderStatus.UNSUCCESSFUL,
+            [HasExactValueRule.NOT_EXACT.format(question=QUESTION_ONE)],
+        )
+
+def test_wrong_stream2_diff():
+    with (
+        patch("branch_compare.verify.get_stream1_diff", return_value="12345"),
+        patch("branch_compare.verify.get_stream2_diff", return_value="99999"),
+        loader.load(
+            "specs/base.yml",
+            "start",
+            mock_answers={
+                QUESTION_ONE: "12345",
+                QUESTION_TWO: "98765",
+            }
+        ) as output,
+    ):
+        assert_output(
+            output,
+            GitAutograderStatus.UNSUCCESSFUL,
+            [HasExactValueRule.NOT_EXACT.format(question=QUESTION_TWO)],
+        )
