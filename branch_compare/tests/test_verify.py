9e662fb9d1307d1d0c7326cd7a14db497be1c955
lavanyagarg112 <lavanyagarg@u.nus.edu>
2025-11-08 19:46:32 +0800

    add base test

--- branch_compare/tests/test_verify.py
+++ branch_compare/tests/test_verify.py
@@ -1,6 +1,7 @@
-from git_autograder import GitAutograderTestLoader
+from git_autograder import GitAutograderStatus, GitAutograderTestLoader, assert_output
+from unittest.mock import patch
 
-from ..verify import verify
+from ..verify import verify, QUESTION_ONE, QUESTION_TWO
 
 REPOSITORY_NAME = "branch-compare"
 
@@ -8,5 +9,16 @@ loader = GitAutograderTestLoader(__file__, REPOSITORY_NAME, verify)
 
 
 def test_base():
-    with loader.load("specs/base.yml", "start"):
-        pass
+    with (
+        patch("branch_compare.verify.get_stream1_diff", return_value="12345"),
+        patch("branch_compare.verify.get_stream2_diff", return_value="98765"),
+        loader.load(
+            "specs/base.yml",
+            "start",
+            mock_answers={
+                QUESTION_ONE: "12345",
+                QUESTION_TWO: "98765",
+            }
+        ) as output,
+    ):
+        assert_output(output, GitAutograderStatus.SUCCESSFUL)
