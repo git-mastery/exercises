82b50cabbb75a9af51b10ac62e1a80d65259c14e
jovnc <95868357+jovnc@users.noreply.github.com>
2025-11-10 20:33:33 +0800

    [tags-update] Enhance tagging functionality

--- tags_update/download.py
+++ tags_update/download.py
@@ -1,9 +1,7 @@
-from exercise_utils.cli import run_command
-
-__resources__ = {}
+from exercise_utils.git import tag
 
 
 def setup(verbose: bool = False):    
-    run_command(["git", "tag", "first-update", "HEAD~4"], verbose)
-    run_command(["git", "tag", "april-update"], verbose)
-    run_command(["git", "tag", "may-update"], verbose)
+    tag("first-update", verbose, "HEAD~4")
+    tag("april-update", verbose)
+    tag("may-update", verbose)
--- tags_update/tests/test_verify.py
+++ tags_update/tests/test_verify.py
@@ -17,7 +17,6 @@ loader = GitAutograderTestLoader(__file__, REPOSITORY_NAME, verify)
 
 def test_base():
     with loader.load("specs/base.yml", "start") as output:
-        print(output)
         assert_output(output, GitAutograderStatus.SUCCESSFUL, [SUCCESS_MESSAGE])
 
 
