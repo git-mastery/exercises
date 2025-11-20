f8dc2f33c94ef6c7fcc4259d67862ef1c0060dc8
vanphuc <e1375522@u.nus.edu>
2025-11-20 12:09:48 +0800

    Fix all the commnet

--- hands_on/sync_upstream.py
+++ hands_on/sync_upstream.py
@@ -1,12 +1,12 @@
 import os
-import sys
-import shutil
 
+from exercise_utils.git import add_remote
 from exercise_utils.github_cli import (
     get_github_username,
     fork_repo,
     clone_repo,
     has_repo,
+    delete_repo
 )
 
 __requires_git__ = True
@@ -14,8 +14,7 @@ __requires_github__ = True
 
 
 TARGET_REPO = "git-mastery/samplerepo-finances"
-LOCAL_DIR = "samplerepo-finances"
-
+LOCAL_DIR = "gitmastery-samplerepo-finances"
 
 def _get_full_repo_name(verbose: bool) -> str:
     username = get_github_username(verbose)
@@ -26,19 +25,11 @@ def download(verbose: bool):
     full_repo_name = _get_full_repo_name(verbose)
 
     if has_repo(full_repo_name, is_fork=True, verbose=verbose):
-        print(
-            f"\nRepository 'https://github.com/{full_repo_name}' already exists on GitHub.\n"
-            "Please delete the existing fork before proceeding."
-        )
-        sys.exit(1)
+        delete_repo(full_repo_name, verbose)
 
-    if os.path.exists(LOCAL_DIR):
-        shutil.rmtree(LOCAL_DIR)
-
-    fork_repo(TARGET_REPO, fork_name="samplerepo-finances", verbose=verbose)
+    fork_repo(TARGET_REPO, fork_name=LOCAL_DIR, verbose=verbose)
 
     clone_repo(full_repo_name, verbose=verbose)
 
     os.chdir(LOCAL_DIR)
-    os.system(f"git remote add upstream https://github.com/{TARGET_REPO}")
-
+    add_remote("origin", f"https://github.com/{full_repo_name}", verbose)
