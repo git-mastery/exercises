0b48ebf9a80e55d12c69a5e5b5091e06a57c0dce
dingZvel <ding_zhe@u.nus.edu>
2025-11-20 19:43:18 +0800

    Refactor using core util functions for gh

--- hands_on/push_tags.py
+++ hands_on/push_tags.py
@@ -1,68 +1,59 @@
 import os
-import subprocess
-from exercise_utils.cli import run_command
+from exercise_utils.cli import run
 from exercise_utils.git import tag_with_options, annotated_tag_with_options
+from exercise_utils.github_cli import clone_repo_with_gh, fork_repo, get_github_username, has_repo
 
 __requires_git__ = True
 __requires_github__ = True
 
-REPO_NAME = "samplerepo-preferences"
-
-def get_username() -> str:
-    # Cannot use run_command function because returned object should not be None.
-    return subprocess.run(
-        ["gh", "api", "user", "-q", ".login"],
-        capture_output=True,
-        text=True,
-    ).stdout.strip()
-
-
-def check_same_repo_name(username: str, repo_name: str) -> None:
-    # Cannot use run_command function because return code is needed.
-    check_repo = subprocess.run(
-        ["gh", "repo", "view", f"{username}/{repo_name}"],
-        capture_output=True,
-        text=True,
+def check_existing_fork(username: str, fork_owner_name: str, repo_name: str, verbose: bool) -> None:
+    result = run(
+        ["gh",
+        "api",
+        f"repos/{fork_owner_name}/{repo_name}/forks",
+        "-q",
+        f'''.[] | .owner.login | select(. =="{username}")''',
+        ],
+        verbose
     )
-    if check_repo.returncode == 0:
-        print(f"Warning: {username}/{REPO_NAME} already exists, the fork repo will be "
-              f"named as {username}/{REPO_NAME}-1")
-
-def check_existing_fork(username: str, fork_owner_name: str, repo_name: str) -> None:
-    try:
-        result = subprocess.run(
-            ["gh",
-            "api",
-            f"repos/{fork_owner_name}/{repo_name}/forks",
-            "-q",
-            f'''.[] | .owner.login | select(. =="{username}")''',
-            ],
-            capture_output=True,
-            text=True,
-            check=True,
-        )
+    if result.is_success():
         if result.stdout == username:
             print(f"ERROR: A fork of {fork_owner_name}/{repo_name} already exists! "
                 "Please delete the fork and run this download operation again.\n"
                 "!Aborting...")
             exit(1)
-    except subprocess.CalledProcessError as e:
-        print(e.stderr)
-        exit(1)
 
+def check_same_repo_name(username: str, repo_name: str, verbose: bool) -> str:
+    check_repo = run(
+        ["gh", "repo", "view", f"{username}/{repo_name}"],
+        verbose
+    )
+    if check_repo.is_success():
+        print(f"Warning: {username}/{repo_name} already exists, the fork repo will be "
+            f"named as {username}/{repo_name}-1")
+        return repo_name + "-1"
+    return repo_name
 
-
+    # if has_repo(repo_name, False, verbose):
+    #     print(f"Warning: {username}/{repo_name} already exists, the fork repo will be "
+    #           f"named as {username}/{repo_name}-1")
+    #     return repo_name + "-1"
+    # return repo_name
 
 def download(verbose: bool):
-    username = get_username()
-    check_existing_fork(username, "git-mastery", REPO_NAME)
-    check_same_repo_name(username, REPO_NAME)
+    REPO_NAME = "samplerepo-preferences"
+    FORK_NAME = "gitmastery-samplerepo-preferences"
+    username = get_github_username(verbose)
 
-    run_command(["gh", "repo", "fork", f"git-mastery/{REPO_NAME}", REPO_NAME, "--clone"], verbose)
+    check_existing_fork(username, "git-mastery", REPO_NAME, verbose)
+    FORK_NAME = check_same_repo_name(username, FORK_NAME, verbose)
 
-    os.chdir(REPO_NAME)
+    fork_repo(f"git-mastery/{REPO_NAME}", FORK_NAME, verbose)
+    clone_repo_with_gh(FORK_NAME, verbose)
+
+
+    os.chdir(FORK_NAME)
 
     tag_with_options("v1.0", ["HEAD~1"], verbose)
     annotated_tag_with_options("v0.9", ["HEAD~2", "-m", "First beta release"], verbose)
 
-    pass
