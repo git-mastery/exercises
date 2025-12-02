9f15849c71a52084e04d577a59e26df582274418
Vikram Goyal <vikramgoyalsg@gmail.com>
2025-12-02 13:30:20 +0800

    Fix formatting

--- tags_push/download.py
+++ tags_push/download.py
@@ -1,6 +1,5 @@
 from exercise_utils.cli import run_command
 from exercise_utils.git import tag
-from exercise_utils.gitmastery import create_start_tag
 
 def setup(verbose: bool = False):
     run_command(["git", "remote", "rename", "origin", "production"], verbose)
@@ -9,4 +8,4 @@ def setup(verbose: bool = False):
     run_command(["git", "tag", "-d", "beta"], verbose)
 
     run_command(["git", "tag", "v1.0", "HEAD~4"], verbose)
-    run_command(["git", "tag", "-a", "v2.0", "HEAD~1", "-m", f"\"{"First stable roster"}\""], verbose)
+    run_command(["git", "tag", "-a", "v2.0", "HEAD~1", "-m", "First stable roster"], verbose)
