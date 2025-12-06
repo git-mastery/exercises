e064be9c3eaaae04062b7ac2a2f21b65173a4ec1
LeeZeHao <zehaolee2004@gmail.com>
2025-12-06 23:34:16 +0800

    Change command call to new clone_repo_with_git util function

--- hands_on/fetch_merge.py
+++ hands_on/fetch_merge.py
@@ -1,14 +1,17 @@
 import os
 
 from exercise_utils.cli import run_command
-from exercise_utils.gitmastery import create_start_tag
+from exercise_utils.git import clone_repo_with_git
 
 __requires_git__ = True
 __requires_github__ = False
 
 
 def download(verbose: bool):
-    os.makedirs("samplerepo-finances")
-    run_command(["git", "clone", "https://github.com/git-mastery/samplerepo-finances.git"], verbose)
+    clone_repo_with_git(
+        "https://github.com/git-mastery/samplerepo-finances.git", verbose
+    )
     os.chdir("samplerepo-finances")
-    run_command(["git", "remote", "set-url", "origin", "https://github.com/git-mastery/samplerepo-finances-2.git"], verbose)
+    run_command(
+        ["git", "remote", "set-url", "origin", "https://github.com/se-edu/samplerepo-finances-2.git"], verbose
+    )
