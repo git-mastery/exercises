e39ecf5d7d09598f0b8f599c496942cd08cd3089
Vikram Goyal <vikramgoyalsg@gmail.com>
2025-12-10 15:08:07 +0800

    Clean up autograding

--- merge_squash/tests/test_verify.py
+++ merge_squash/tests/test_verify.py
@@ -16,13 +16,16 @@ def test_base():
     with loader.load("specs/base.yml") as output:
         assert_output(output, GitAutograderStatus.SUCCESSFUL)
 
+
 def test_non_squash_merge_used():
     with loader.load("specs/non_squash_merge_used.yml") as output:
         assert_output(output, GitAutograderStatus.UNSUCCESSFUL, [SQUASH_NOT_USED])
 
+
 def test_not_merged():
     with loader.load("specs/not_merged.yml") as output:
         assert_output(output, GitAutograderStatus.UNSUCCESSFUL, [CHANGES_FROM_SUPPORTING_NOT_PRESENT])
+
 
 def test_missing_main_commits():
     with loader.load("specs/missing_main_commits.yml") as output:
--- merge_squash/verify.py
+++ merge_squash/verify.py
@@ -4,10 +4,6 @@ from git_autograder import (
     GitAutograderStatus,
 )
 
-ADD_JOEY = "Add Joey"
-ADD_PHOEBE = "Add Phoebe"
-ADD_ROSS = "Add Ross"
-
 SQUASH_NOT_USED = (
     "You should be using squash merge, not regular merge."
 )
@@ -28,11 +24,6 @@ def verify(exercise: GitAutograderExercise) -> GitAutograderOutput:
 
     merge_commits = [c for c in main_branch.commits if len(c.parents) > 1]
     if merge_commits:
-        print("Yo" + merge_commits[0].commit.message)
-        print(commit_messages_in_main)
-        print(merge_commits[0].parents[0].commit.message)
-        print(merge_commits[0].parents[1].commit.message)
-        print(main_branch.commits[0].stats.files)
         raise exercise.wrong_answer([SQUASH_NOT_USED])
 
     with exercise.repo.files.file_or_none("mike.txt") as mike_file:
@@ -44,7 +35,7 @@ def verify(exercise: GitAutograderExercise) -> GitAutograderOutput:
             raise exercise.wrong_answer([CHANGES_FROM_SUPPORTING_NOT_PRESENT])
 
     if not all(
-        msg in commit_messages_in_main for msg in [ADD_JOEY, ADD_PHOEBE, ADD_ROSS]
+        msg in commit_messages_in_main for msg in ["Add Joey", "Add Phoebe", "Add Ross"]
     ):
         raise exercise.wrong_answer([MAIN_COMMITS_INCORRECT])
 
