dc4ad65ac1b2398a3e750ddb0bfed6e21dc83dd0
Vikram Goyal <vikramgoyalsg@gmail.com>
2025-12-10 14:32:58 +0800

    Add autograding (incomplete) There seems to be some issue with the squash option for the merge step in repo-smith

--- merge_squash/README.md
+++ merge_squash/README.md
@@ -1,18 +1,40 @@
 # merge-squash
 
-<!--- Insert exercise description -->
+You are keeping notes on the cast of a sitcom you've started watching. Initially, you kept main cast and supporting cast on two separate branches.
+
+```mermaid
+gitGraph BT:
+    commit id: "Add Joey"
+    commit id: "Add Phoebe"
+    branch supporting
+    checkout supporting
+    commit id: "Add Mike"
+    commit id: "Add Janice"
+    checkout main
+    commit id: "Add Ross"
+```
+
+Now you wish to keep everything in the `main` branch.
 
 ## Task
 
-<!--- Insert exercise task, simplify what needs to be done -->
+Squash-merge the `supporting` branch onto the `main` branch.
+
+The result should look as follows:
+
+```mermaid
+gitGraph BT:
+    commit id: "Add Joey"
+    commit id: "Add Phoebe"
+    branch supporting
+    checkout supporting
+    commit id: "Add Mike"
+    commit id: "Add Janice"
+    checkout main
+    commit id: "Add Ross"
+    commit id: "Squash commit"
+```
 
 ## Hints
 
-<!--- Insert hints here -->
-<!---
-    Use Github Markdown's collapsible content:
-    <details>
-    <summary>...</summary>
-    ...
-    </details>
--->
+You have to commit manually after performing the squash merge.
--- merge_squash/verify.py
+++ merge_squash/verify.py
@@ -4,8 +4,48 @@ from git_autograder import (
     GitAutograderStatus,
 )
 
+ADD_JOEY = "Add Joey"
+ADD_PHOEBE = "Add Phoebe"
+ADD_ROSS = "Add Ross"
+
+SQUASH_NOT_USED = (
+    "You should be using squash merge, not regular merge."
+)
+
+MAIN_COMMITS_INCORRECT = (
+    "The main branch does not contain at least one of these commits: 'Add Joey', 'Add Phoebe' or 'Add Ross'."
+)
+
+CHANGES_FROM_SUPPORTING_NOT_PRESENT = (
+    "The main branch does not contain both files 'mike.txt' and 'janice.txt'."
+)
+
 
 def verify(exercise: GitAutograderExercise) -> GitAutograderOutput:
-    # INSERT YOUR GRADING CODE HERE
+    main_branch = exercise.repo.branches.branch("main")
+
+    commit_messages_in_main = [c.commit.message.strip() for c in main_branch.commits]
+
+    merge_commits = [c for c in main_branch.commits if len(c.parents) > 1]
+    if merge_commits:
+        print("Yo" + merge_commits[0].commit.message)
+        print(commit_messages_in_main)
+        print(merge_commits[0].parents[0].commit.message)
+        print(merge_commits[0].parents[1].commit.message)
+        print(main_branch.commits[0].stats.files)
+        raise exercise.wrong_answer([SQUASH_NOT_USED])
+
+    with exercise.repo.files.file_or_none("mike.txt") as mike_file:
+        if mike_file is None:
+            raise exercise.wrong_answer([CHANGES_FROM_SUPPORTING_NOT_PRESENT])
+
+    with exercise.repo.files.file_or_none("janice.txt") as janice_file:
+        if janice_file is None:
+            raise exercise.wrong_answer([CHANGES_FROM_SUPPORTING_NOT_PRESENT])
+
+    if not all(
+        msg in commit_messages_in_main for msg in [ADD_JOEY, ADD_PHOEBE, ADD_ROSS]
+    ):
+        raise exercise.wrong_answer([MAIN_COMMITS_INCORRECT])
 
     return exercise.to_output([], GitAutograderStatus.SUCCESSFUL)
