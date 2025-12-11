2130f24193d1404d301c34b3744baa3fa4ee7a05
jia xin <122351681+jiaxinnns@users.noreply.github.com>
2025-12-11 21:55:12 +0800

    [hp-sync-upstream] Enhance hands on hp-sync-upstream (#152)
    # Exercise Review
    
    ## Exercise Discussion
    
    Fixes https://github.com/git-mastery/exercises/issues/135
    
    ## Checklist
    
    - [ ] If you require a new remote repository on the `Git-Mastery`
    organization, have you [created a
    request](https://github.com/git-mastery/exercises/issues/new?template=request_exercise_repository.md)
    for it?
    - [ ] Have you written unit tests using
    [`repo-smith`](https://github.com/git-mastery/repo-smith) to validate
    the exercise grading scheme?
    - [x] Have you tested the download script using `test-download.sh`?
    - [x] Have you verified that this exercise does not already exist or is
    not currently in review?
    - [ ] Did you introduce a new grading mechanism that should belong to
    [`git-autograder`](https://github.com/git-mastery/git-autograder)?
    - [ ] Did you introduce a new dependency that should belong to
    [`app`](https://github.com/git-mastery/app)?

--- hands_on/sync_upstream.py
+++ hands_on/sync_upstream.py
@@ -1,10 +1,11 @@
 import os
 
+from exercise_utils.cli import run_command
 from exercise_utils.git import add_remote
 from exercise_utils.github_cli import (
     get_github_username,
     fork_repo,
-    clone_repo,
+    clone_repo_with_gh,
     has_repo,
     delete_repo,
 )
@@ -13,20 +14,24 @@ __requires_git__ = True
 __requires_github__ = True
 
 
-TARGET_REPO = "git-mastery/samplerepo-finances"
-LOCAL_DIR = "gitmastery-samplerepo-finances"
+TARGET_REPO = "git-mastery/samplerepo-finances-2"
+FORK_NAME = "gm-samplerepo-finances-2"
+LOCAL_DIR = "samplerepo-finances"
 
 
 def download(verbose: bool):
     username = get_github_username(verbose)
-    full_repo_name = f"{username}/{LOCAL_DIR}"
+    full_repo_name = f"{username}/{FORK_NAME}"
 
     if has_repo(full_repo_name, True, verbose):
         delete_repo(full_repo_name, verbose)
 
-    fork_repo(TARGET_REPO, LOCAL_DIR, verbose)
+    fork_repo(TARGET_REPO, FORK_NAME, verbose)
 
-    clone_repo(full_repo_name, verbose)
+    clone_repo_with_gh(full_repo_name, verbose, LOCAL_DIR)
 
     os.chdir(LOCAL_DIR)
-    add_remote("origin", f"https://github.com/{full_repo_name}", verbose)
+
+    run_command(["git", "reset", "--hard", "HEAD~2"], verbose)
+    run_command(["git", "push", "-f", "origin", "master"], verbose)
+    
