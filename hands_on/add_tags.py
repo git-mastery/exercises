0633878108d048fd1de8c19f931ad4b78fa03819
jovnc <95868357+jovnc@users.noreply.github.com>
2025-12-12 16:04:59 +0800

    [hp-add-tags] Align method to fork repository

--- hands_on/add_tags.py
+++ hands_on/add_tags.py
@@ -1,11 +1,25 @@
-from exercise_utils.cli import run_command
+from exercise_utils.github_cli import (
+    get_github_username,
+    has_repo,
+    fork_repo,
+    delete_repo,
+    clone_repo_with_gh,
+)
 
 __requires_git__ = True
 __requires_github__ = True
 
-WORKING_REPO = "samplerepo-preferences"
+
+TARGET_REPO = "git-mastery/samplerepo-preferences"
+LOCAL_DIR = "gitmastery-samplerepo-preferences"
 
 
 def download(verbose: bool):
-    run_command(["gh", "repo", "fork", f"git-mastery/{WORKING_REPO}",
-                 "--clone"], verbose)
+    username = get_github_username(verbose)
+    full_repo_name = f"{username}/{LOCAL_DIR}"
+
+    if has_repo(full_repo_name, True, verbose):
+        delete_repo(full_repo_name, verbose)
+
+    fork_repo(TARGET_REPO, LOCAL_DIR, verbose)
+    clone_repo_with_gh(full_repo_name, verbose)
