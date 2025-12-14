18daad5a18adb4bb447b7fd4c5d20b9128663bf0
Vikram Goyal <vikramgoyalsg@gmail.com>
2025-12-14 17:27:39 +0800

    Implement hp-merge-conflicts

--- hands_on/merge_conflicts.py
+++ hands_on/merge_conflicts.py
@@ -1,9 +1,33 @@
-from exercise_utils.cli import run_command
-from exercise_utils.gitmastery import create_start_tag
+import os
+
+from exercise_utils.file import create_or_update_file, append_to_file
+from exercise_utils.git import add, commit, init, checkout
 
 __requires_git__ = True
 __requires_github__ = False
 
 
 def download(verbose: bool):
-    pass
+    os.makedirs("nouns")
+    os.chdir("nouns")
+
+    init(verbose)
+
+    create_or_update_file(
+        "colours.txt",
+        """
+        blue
+        """,
+    )
+    add(["colours.txt"], verbose)
+    commit("Add colours.txt", verbose)
+
+    checkout("fix1", True, verbose)
+    append_to_file("colours.txt", "green\nred\nwhite")
+    add(["colours.txt"], verbose)
+    commit("Add green, red, white", verbose)
+
+    checkout("main", False, verbose)
+    append_to_file("colours.txt", "black\nred\nwhite")
+    add(["colours.txt"], verbose)
+    commit("Add black, red, white", verbose)
