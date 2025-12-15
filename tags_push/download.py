d9bf436f73846a78b94599a052c399c346a7a9dc
Vikram Goyal <vikramgoyalsg@gmail.com>
2025-12-15 17:41:45 +0800

    Refactor download.py to use tag_with_options

--- tags_push/download.py
+++ tags_push/download.py
@@ -1,5 +1,5 @@
 from exercise_utils.cli import run_command
-from exercise_utils.git import tag
+from exercise_utils.git import tag, tag_with_options
 
 def setup(verbose: bool = False):
     run_command(["git", "remote", "rename", "origin", "production"], verbose)
@@ -7,5 +7,5 @@ def setup(verbose: bool = False):
     run_command(["git", "push", "production", "--tags"], verbose)
     run_command(["git", "tag", "-d", "beta"], verbose)
 
-    run_command(["git", "tag", "v1.0", "HEAD~4"], verbose)
-    run_command(["git", "tag", "-a", "v2.0", "HEAD~1", "-m", "First stable roster"], verbose)
+    tag_with_options("v1.0", ["HEAD~4"], verbose)
+    tag_with_options("v2.0", ["-a", "HEAD~1", "-m", "First stable roster"], verbose)
