2f717994b1ebd4481f798901e075fed25420ee87
Van Phuc <e1375522@u.nus.edu>
2025-12-16 11:44:24 +0700

    [ff-undo] Implement exercise T6L2/ff-undo (#101)
    # Exercise Review
    
    ## Exercise Discussion
    
    #89
    
    ## Checklist
    
    - [ ] If you require a new remote repository on the `Git-Mastery`
    organization, have you [created a
    request](https://github.com/git-mastery/exercises/issues/new?template=request_exercise_repository.md)
    for it?
    - [X] Have you written unit tests using
    [`repo-smith`](https://github.com/git-mastery/repo-smith) to validate
    the exercise grading scheme?
    - [X] Have you tested the download script using `test-download.sh`?
    - [X] Have you verified that this exercise does not already exist or is
    not currently in review?
    - [ ] Did you introduce a new grading mechanism that should belong to
    [`git-autograder`](https://github.com/git-mastery/git-autograder)?
    - [ ] Did you introduce a new dependency that should belong to
    [`app`](https://github.com/git-mastery/app)?
    
    ---------
    
    Co-authored-by: jovnc <95868357+jovnc@users.noreply.github.com>

---
+++ ff_undo/tests/test_verify.py
@@ -0,0 +1,42 @@
+from git_autograder import GitAutograderTestLoader, assert_output
+from git_autograder.status import GitAutograderStatus
+from ..verify import (
+    MERGE_NOT_UNDONE,
+    MAIN_COMMITS_INCORRECT,
+    OTHERS_COMMITS_INCORRECT,
+    OTHERS_BRANCH_MISSING,
+    verify,
+)
+
+REPOSITORY_NAME = "ff-undo"
+
+loader = GitAutograderTestLoader(__file__, REPOSITORY_NAME, verify)
+
+
+def test_correct_solution():
+    with loader.load("specs/base.yml") as output:
+        assert_output(output, GitAutograderStatus.SUCCESSFUL)
+
+
+def test_merge_not_undone():
+    with loader.load("specs/merge_not_undone.yml") as output:
+        assert_output(output, GitAutograderStatus.UNSUCCESSFUL, [MERGE_NOT_UNDONE])
+
+
+def test_branch_missing():
+    with loader.load("specs/branch_missing.yml") as output:
+        assert_output(output, GitAutograderStatus.UNSUCCESSFUL, [OTHERS_BRANCH_MISSING])
+
+
+def test_main_commits_incorrect():
+    with loader.load("specs/main_commits_incorrect.yml") as output:
+        assert_output(
+            output, GitAutograderStatus.UNSUCCESSFUL, [MAIN_COMMITS_INCORRECT]
+        )
+
+
+def test_others_commits_incorrect():
+    with loader.load("specs/others_commits_incorrect.yml") as output:
+        assert_output(
+            output, GitAutograderStatus.UNSUCCESSFUL, [OTHERS_COMMITS_INCORRECT]
+        )
