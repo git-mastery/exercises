704a6680c6fff003897330d7e66f36a9b72f632a
jovnc <95868357+jovnc@users.noreply.github.com>
2025-12-18 19:08:05 +0800

    [hp-fetch-merge] Fix download does not match manual setup

--- hands_on/fetch_merge.py
+++ hands_on/fetch_merge.py
@@ -1,6 +1,7 @@
+from exercise_utils.cli import run_command
 import os
 
-from exercise_utils.git import add_remote, clone_repo_with_git, remove_remote
+from exercise_utils.git import clone_repo_with_git
 
 __requires_git__ = True
 __requires_github__ = False
@@ -11,7 +12,4 @@ def download(verbose: bool):
         "https://github.com/git-mastery/samplerepo-finances.git", verbose
     )
     os.chdir("samplerepo-finances")
-    remove_remote("origin", verbose)
-    add_remote(
-        "origin", "https://github.com/git-mastery/samplerepo-finances-2", verbose
-    )
+    run_command(["git", "remote", "set-url", "origin", "https://github.com/git-mastery/samplerepo-finances-2.git"], verbose)
