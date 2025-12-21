a072bd9d901797c223847659d2db10a25839f2da
jovnc <95868357+jovnc@users.noreply.github.com>
2025-12-21 16:25:52 +0800

    [hp-pull-remote] Fix origin remote URL configuration

--- hands_on/pull_remote.py
+++ hands_on/pull_remote.py
@@ -1,6 +1,7 @@
 import os
 
-from exercise_utils.git import add_remote, clone_repo_with_git
+from exercise_utils.git import clone_repo_with_git
+from exercise_utils.cli import run_command
 
 __requires_git__ = True
 __requires_github__ = False
@@ -12,6 +13,13 @@ def download(verbose: bool):
         "https://github.com/git-mastery/samplerepo-finances.git", verbose
     )
     os.chdir("samplerepo-finances")
-    add_remote(
-        "origin", "https://github.com/git-mastery/samplerepo-finances-2.git", verbose
+    run_command(
+        [
+            "git",
+            "remote",
+            "set-url",
+            "origin",
+            "https://github.com/git-mastery/samplerepo-finances-2.git",
+        ],
+        verbose,
     )
