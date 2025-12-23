8a9c035badd960ff0395cef6afa818355982cffe
Vikram Goyal <vikramgoyalsg@gmail.com>
2025-12-23 22:43:37 +0800

    Refacotr to use clone_repo_with_git

--- hands_on/remote_branch_rename.py
+++ hands_on/remote_branch_rename.py
@@ -1,12 +1,12 @@
 import os
 
 from exercise_utils.cli import run_command
+from exercise_utils.git import clone_repo_with_git
 from exercise_utils.github_cli import (
     get_github_username,
     has_repo,
     fork_repo_all_branches,
     delete_repo,
-    clone_repo_with_gh,
 )
 
 __requires_git__ = True
@@ -19,12 +19,13 @@ LOCAL_DIR = "gitmastery-samplerepo-books"
 def download(verbose: bool):
     username = get_github_username(verbose)
     full_repo_name = f"{username}/{LOCAL_DIR}"
+    repo_url = f"https://github.com/{full_repo_name}"
 
     if has_repo(full_repo_name, True, verbose):
         delete_repo(full_repo_name, verbose)
 
     fork_repo_all_branches(TARGET_REPO, LOCAL_DIR, verbose)
-    clone_repo_with_gh(full_repo_name, verbose, "samplerepo-books")
+    clone_repo_with_git(repo_url, verbose, "samplerepo-books")
 
     os.chdir("samplerepo-books")
     run_command(["git", "switch", "-c", "fantasy", "origin/fantasy"], verbose)
