f8041199a54a4215387af7fd0e55b4b71008e593
jovnc <95868357+jovnc@users.noreply.github.com>
2025-12-24 00:51:23 +0800

    [sensors-reset] Refactor verify logic

--- sensors_reset/verify.py
+++ sensors_reset/verify.py
@@ -1,3 +1,4 @@
+from typing import List
 from git_autograder import (
     GitAutograderOutput,
     GitAutograderExercise,
@@ -11,28 +12,31 @@ CONTAINS_TASK_TWO_COMMIT = (
 CONTAINS_TASK_THREE_COMMIT = (
     "It seems like the commit from Jan 12 is still present in the commit history."
 )
-TASK_ONE_WRONG_RESET = "Changes from Jan 14 and Jan 15 should not be present in the working directory or staging area."
-TASK_TWO_WRONG_RESET = "Changes from Jan 13 should be present in the working directory but not in the staging area."
-TASK_THREE_WRONG_RESET = "Changes from Jan 12 should be present in both the working directory and staging area."
+WRONG_FILES_IN_STAGING_AREA = (
+    "Unexpected files in staging area. Looks like you used the wrong reset mode."
+)
+WRONG_FILES_IN_WORKING_DIRECTORY = (
+    "Unexpected files in working directory. Looks like you used the wrong reset mode."
+)
 
 
-def get_staged_files(exercise: GitAutograderExercise) -> set:
-    """Get files that are staged."""
-    staged_diff = exercise.repo.repo.index.diff("HEAD")
-    return {diff_item.a_path for diff_item in staged_diff}
+def get_staged_files(exercise: GitAutograderExercise) -> List[str | None]:
+    """Get files that are staged (differ between index and HEAD)."""
+    repo = exercise.repo.repo
+    return [diff_item.a_path for diff_item in repo.head.commit.diff()]
 
 
-def get_unstaged_files(exercise: GitAutograderExercise) -> set:
-    """Get files that have unstaged changes."""
-    unstaged_diff = exercise.repo.repo.index.diff(None)
-    return {diff_item.a_path for diff_item in unstaged_diff}
+def get_unstaged_files(exercise: GitAutograderExercise) -> List[str | None]:
+    """Get files that have unstaged changes (differ between working tree and index)."""
+    repo = exercise.repo.repo
+    return [d.a_path for d in repo.index.diff(None)]
 
 
 def verify(exercise: GitAutograderExercise) -> GitAutograderOutput:
-    master_branch = exercise.repo.branches.branch_or_none(
-        "master"
-    ) or exercise.repo.branches.branch_or_none("main")
-    commit_messages = [str(c.commit.message) for c in master_branch.commits]
+    branch = exercise.repo.branches.branch_or_none(
+        "main"
+    ) or exercise.repo.branches.branch_or_none("master")
+    commit_messages = [str(c.commit.message.strip()) for c in branch.commits]
 
     staged_files = get_staged_files(exercise)
     unstaged_files = get_unstaged_files(exercise)
@@ -51,15 +55,26 @@ def verify(exercise: GitAutograderExercise) -> GitAutograderOutput:
     if "Record data for Jan 13" in commit_messages:
         raise exercise.wrong_answer([CONTAINS_TASK_TWO_COMMIT])
 
-    if not unstaged_files:
-        raise exercise.wrong_answer([TASK_TWO_WRONG_RESET])
-
     # Task 3: Commit should be removed, changes should be in staging area
     if "Record data for Jan 12" in commit_messages:
         raise exercise.wrong_answer([CONTAINS_TASK_THREE_COMMIT])
 
-    if not staged_files:
-        raise exercise.wrong_answer([TASK_THREE_WRONG_RESET])
+    # TODO: need a way to verify task by task
+    # currently we verify staging area and working directory of all tasks at once, since we cannot get state of each task
+    comments: str = []
+    if len(unstaged_files) != 4 or not all(
+        file in unstaged_files
+        for file in ["east.csv", "north.csv", "south.csv", "west.csv"]
+    ):
+        comments.append(WRONG_FILES_IN_WORKING_DIRECTORY)
+
+    if len(staged_files) != 3 or not all(
+        file in staged_files for file in ["east.csv", "north.csv", "south.csv"]
+    ):
+        comments.append(WRONG_FILES_IN_STAGING_AREA)
+
+    if comments:
+        raise exercise.wrong_answer(comments)
 
     return exercise.to_output(
         ["You have reset the repository to the correct state!"],
