79eceb684809c79b08788793abcffe4789bb7f57
jovnc <95868357+jovnc@users.noreply.github.com>
2025-12-24 00:51:40 +0800

    [sensors-reset] Update repo-smith tests with new verify logic

--- sensors_reset/tests/test_verify.py
+++ sensors_reset/tests/test_verify.py
@@ -1,6 +1,10 @@
-from sensors_reset.verify import CONTAINS_TASK_THREE_COMMIT
-from sensors_reset.verify import CONTAINS_TASK_TWO_COMMIT
-from sensors_reset.verify import CONTAINS_TASK_ONE_COMMITS
+from sensors_reset.verify import (
+    CONTAINS_TASK_ONE_COMMITS,
+    CONTAINS_TASK_TWO_COMMIT,
+    CONTAINS_TASK_THREE_COMMIT,
+    WRONG_FILES_IN_STAGING_AREA,
+    WRONG_FILES_IN_WORKING_DIRECTORY,
+)
 from git_autograder.status import GitAutograderStatus
 from git_autograder.test_utils import assert_output
 from git_autograder import GitAutograderTestLoader
@@ -14,37 +18,43 @@ loader = GitAutograderTestLoader(__file__, REPOSITORY_NAME, verify)
 
 def test_base():
     with loader.load("specs/base.yml", "start") as output:
-        # assert_output(output, GitAutograderStatus.SUCCESSFUL)
-        pass
+        assert_output(output, GitAutograderStatus.SUCCESSFUL)
 
 
 def test_no_changes():
     with loader.load("specs/no_changes.yml", "start") as output:
-        # assert_output(
-        #     output, GitAutograderStatus.UNSUCCESSFUL, [CONTAINS_TASK_ONE_COMMITS]
-        # )
-        pass
+        assert_output(
+            output, GitAutograderStatus.UNSUCCESSFUL, [CONTAINS_TASK_ONE_COMMITS]
+        )
 
 
-def test_wrong_task_one_reset():
-    with loader.load("specs/wrong_task_one_reset.yml", "start") as output:
-        # assert_output(
-        #     output, GitAutograderStatus.UNSUCCESSFUL, [CONTAINS_TASK_ONE_COMMITS]
-        # )
-        pass
+def test_incomplete_task_two_reset():
+    with loader.load("specs/incomplete_task_two_reset.yml", "start") as output:
+        assert_output(
+            output, GitAutograderStatus.UNSUCCESSFUL, [CONTAINS_TASK_TWO_COMMIT]
+        )
+
+
+def test_incomplete_task_three_reset():
+    with loader.load("specs/incomplete_task_three_reset.yml", "start") as output:
+        assert_output(
+            output, GitAutograderStatus.UNSUCCESSFUL, [CONTAINS_TASK_THREE_COMMIT]
+        )
 
 
 def test_wrong_task_two_reset():
     with loader.load("specs/wrong_task_two_reset.yml", "start") as output:
-        # assert_output(
-        #     output, GitAutograderStatus.UNSUCCESSFUL, [CONTAINS_TASK_TWO_COMMIT]
-        # )
-        pass
+        assert_output(
+            output,
+            GitAutograderStatus.UNSUCCESSFUL,
+            [WRONG_FILES_IN_WORKING_DIRECTORY],
+        )
 
 
 def test_wrong_task_three_reset():
     with loader.load("specs/wrong_task_three_reset.yml", "start") as output:
-        # assert_output(
-        #     output, GitAutograderStatus.UNSUCCESSFUL, [CONTAINS_TASK_THREE_COMMIT]
-        # )
-        pass
+        assert_output(
+            output,
+            GitAutograderStatus.UNSUCCESSFUL,
+            [WRONG_FILES_IN_WORKING_DIRECTORY, WRONG_FILES_IN_STAGING_AREA],
+        )
