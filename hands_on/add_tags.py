e70f1b2889bb68baeca1eab5fba1c90f05f0bab8
Jiahao, Woo <woojiahao1234@gmail.com>
2025-12-30 19:03:39 +0800

    [hp-add-tags] Migrate to repo-smith

--- hands_on/add_tags.py
+++ hands_on/add_tags.py
@@ -1,10 +1,8 @@
 from exercise_utils.github_cli import (
     get_github_username,
     has_repo,
-    fork_repo,
-    delete_repo,
-    clone_repo_with_gh,
 )
+from repo_smith.repo_smith import RepoSmith
 
 __requires_git__ = True
 __requires_github__ = True
@@ -14,12 +12,16 @@ TARGET_REPO = "git-mastery/samplerepo-preferences"
 LOCAL_DIR = "gitmastery-samplerepo-preferences"
 
 
-def download(verbose: bool):
-    username = get_github_username(verbose)
-    full_repo_name = f"{username}/{LOCAL_DIR}"
+def download(rs: RepoSmith):
+    username = get_github_username(rs)
 
-    if has_repo(full_repo_name, True, verbose):
-        delete_repo(full_repo_name, verbose)
+    if has_repo(rs, username, LOCAL_DIR, is_fork=True):
+        rs.gh.repo_delete(username, LOCAL_DIR)
 
-    fork_repo(TARGET_REPO, LOCAL_DIR, verbose)
-    clone_repo_with_gh(full_repo_name, verbose)
+    rs.gh.repo_fork(
+        "git-mastery",
+        "samplerepo-preferences",
+        default_branch_only=True,
+        fork_name=LOCAL_DIR,
+        clone=True,
+    )
