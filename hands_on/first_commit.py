4cecc50a5ed48b026a41d87cf249cffebf876f6f
Jiahao, Woo <woojiahao1234@gmail.com>
2025-12-30 19:06:55 +0800

    [hp-first-commit] Migrate to repo-smith

--- hands_on/first_commit.py
+++ hands_on/first_commit.py
@@ -1,16 +1,14 @@
-import os
-from exercise_utils.file import create_or_update_file
-from exercise_utils.git import add, init
+from repo_smith.repo_smith import RepoSmith
 
 __requires_git__ = True
 __requires_github__ = False
 
 
-def download(verbose: bool):
-    os.makedirs("things")
-    os.chdir("things")
-    init(verbose)
-    create_or_update_file(
+def download(rs: RepoSmith):
+    rs.files.mkdir("things")
+    rs.files.cd("things")
+    rs.git.init()
+    rs.files.create_or_update(
         "fruits.txt",
         """
         apples
@@ -19,4 +17,4 @@ def download(verbose: bool):
         dragon fruits
         """,
     )
-    add(["fruits.txt"], verbose)
+    rs.git.add(["fruits.txt"])
