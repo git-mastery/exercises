568337baf09cc8fbb9e7b3e6f57644496c4a528b
desmondwong1215 <desmondwong1215@gmail.com>
2026-01-19 13:54:22 +0800

    Implement setup logic

--- glossary_branch_pull/download.py
+++ glossary_branch_pull/download.py
@@ -1,8 +1,5 @@
-import os
-
 from exercise_utils.cli import run_command
 from exercise_utils.file import create_or_update_file
-from exercise_utils.gitmastery import create_start_tag
 from exercise_utils.git import add, checkout, clone_repo_with_git, commit, push
 from exercise_utils.github_cli import (
     get_github_username,
@@ -11,11 +8,10 @@ from exercise_utils.github_cli import (
     delete_repo,
 )
 
-__resources__ = {}
-
 TARGET_REPO = "git-mastery/samplerepo-funny-glossary"
 FORK_NAME = "gitmastery-samplerepo-funny-glossary"
-LOCAL_DIR = "funny-glossary"
+
+__resources__ = {}
 
 
 def setup(verbose: bool = False):
@@ -26,9 +22,7 @@ def setup(verbose: bool = False):
         delete_repo(full_repo_name, verbose)
 
     fork_repo(TARGET_REPO, FORK_NAME, verbose, False)
-    clone_repo_with_git(f"https://github.com/{full_repo_name}", verbose, LOCAL_DIR)
-
-    os.chdir(LOCAL_DIR)
+    clone_repo_with_git(f"https://github.com/{full_repo_name}", verbose, ".")
 
     run_command(["git", "branch", "-dr", "origin/VWX"], verbose)
 
