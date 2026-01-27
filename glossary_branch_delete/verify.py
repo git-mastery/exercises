93f9a816c56f954d61b3808a7a3c9be49c01a512
jia xin <jjiiaaxxiinn@gmail.com>
2026-01-27 15:31:03 +0800

    Add verify logic

--- glossary_branch_delete/verify.py
+++ glossary_branch_delete/verify.py
@@ -4,8 +4,26 @@ from git_autograder import (
     GitAutograderStatus,
 )
 
+VWX_BRANCH_EXISTS_LOCALLY = "Branch 'VWX' still exists locally! Remember to delete it."
+VWX_BRANCH_EXISTS_REMOTELY = (
+    "Branch 'VWX' still exists on the remote! Remember to delete it from the remote."
+)
+
 
 def verify(exercise: GitAutograderExercise) -> GitAutograderOutput:
-    # INSERT YOUR GRADING CODE HERE
+    if exercise.repo.branches.has_branch("VWX"):
+        raise exercise.wrong_answer([VWX_BRANCH_EXISTS_LOCALLY])
 
-    return exercise.to_output([], GitAutograderStatus.SUCCESSFUL)
+    origin_remote = exercise.repo.remotes.remote("origin")
+    origin_remote.remote.fetch()
+
+    try:
+        exercise.repo.repo.refs["origin/VWX"]
+        raise exercise.wrong_answer([VWX_BRANCH_EXISTS_REMOTELY])
+    except (IndexError, KeyError):
+        pass
+
+    return exercise.to_output(
+        ["Great job deleting the VWX branch locally and remotely!"],
+        GitAutograderStatus.SUCCESSFUL,
+    )
