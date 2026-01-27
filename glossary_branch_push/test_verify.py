b22ba314ddb98c001f63b8f51a52282e92685345
jia xin <jjiiaaxxiinn@gmail.com>
2026-01-27 14:53:10 +0800

    Add test code

--- glossary_branch_push/test_verify.py
+++ glossary_branch_push/test_verify.py
@@ -1,7 +1,5 @@
 from contextlib import contextmanager
 from typing import Iterator, Tuple
-import tempfile
-import os
 
 from exercise_utils.test import (
     GitAutograderTest,
@@ -10,7 +8,6 @@ from exercise_utils.test import (
 )
 from git_autograder import GitAutograderStatus
 from repo_smith.repo_smith import RepoSmith
-from repo_smith.steps.bash_step import BashStep
 
 from .verify import PQR_BRANCH_NOT_PUSHED, verify
 
@@ -21,19 +18,17 @@ loader = GitAutograderTestLoader(REPOSITORY_NAME, verify)
 
 @contextmanager
 def base_setup() -> Iterator[Tuple[GitAutograderTest, RepoSmith]]:
-    with loader.start() as (test, rs):
+    with loader.start(include_remote_repo=True) as (test, rs, rs_remote):
         rs.git.commit(message="Initial commit", allow_empty=True)
-        
-        # Create a bare repo to use as remote
-        remote_dir = tempfile.mkdtemp()
-        remote_path = os.path.join(remote_dir, "remote.git")
-        os.makedirs(remote_path)
-        BashStep(name=None, description=None, id=None, body=f"git init --bare {remote_path}").execute(rs.repo)
-        
+
+        remote_path = str(rs_remote.repo.git_dir)
         rs.git.remote_add("origin", remote_path)
-        
+
         rs.git.checkout("PQR", branch=True)
-        rs.files.create_or_update("r.txt", "refactoring: Improving the code without changing what it does... in theory.\n")
+        rs.files.create_or_update(
+            "r.txt",
+            "refactoring: Improving the code without changing what it does... in theory.\n",
+        )
         rs.git.add(all=True)
         rs.git.commit(message="Add 'refactoring'")
         rs.git.checkout("main")
@@ -43,7 +38,7 @@ def base_setup() -> Iterator[Tuple[GitAutograderTest, RepoSmith]]:
 
 def test_base():
     with base_setup() as (test, rs):
-        BashStep(name=None, description=None, id=None, body="git push origin PQR").execute(rs.repo)
+        rs.git.push("origin", "PQR")
 
         output = test.run()
         assert_output(output, GitAutograderStatus.SUCCESSFUL)
