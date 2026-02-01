190bc3a036fb1abed042bf4c619aba01b5516161
desmondwong1215 <desmondwong1215@gmail.com>
2026-02-01 16:43:45 +0800

    Improve verify logic

--- glossary_branch_pull/verify.py
+++ glossary_branch_pull/verify.py
@@ -1,20 +1,31 @@
+from typing import List
 from git_autograder import (
+    GitAutograderCommit,
     GitAutograderOutput,
     GitAutograderExercise,
     GitAutograderStatus,
 )
 
-BRANCH_NOT_CREATED = "The local {branch} branch is not created."
+BRANCH_MISSING = "The local {branch} branch is not created."
 BRANCH_NOT_TRACKING = "The local {branch} branch does not track origin/{branch}."
-BRANCH_MISSING = "The local {branch} branch does not exist."
-COMMIT_MISSING = "New commit in the remote {branch} branch is not pulled to the local {branch} branch."
-LOCAL_COMMIT_MISSING = "The original local commit on DEF is missing. You may have lost your work instead of merging."
+REMOTE_COMMIT_MISSING = "New commit in the remote {branch} branch is not pulled to the local {branch} branch."
+LOCAL_COMMIT_MISSING = "The original local commit on DEF is missing. " \
+"You may have lost your work instead of merging."
 
 
-def get_commit_from_message(commits, message):
+def get_commit_from_message(commits: List[GitAutograderCommit], message: str) \
+    -> GitAutograderCommit | None:
     """Find a commit with the given message from a list of commits."""
     for commit in commits:
-        if message.strip() == commit.message.strip():
+        if message.strip() == commit.commit.message.strip():
+            return commit
+    return None
+
+def get_commit_from_hexsha(commits: List[GitAutograderCommit], hexsha: str) \
+    -> GitAutograderCommit | None:
+    """Find a commit with the given hexsha from a list of commits."""
+    for commit in commits:
+        if hexsha.strip() == commit.commit.hexsha.strip():
             return commit
     return None
 
@@ -23,48 +34,48 @@ def verify(exercise: GitAutograderExercise) -> GitAutograderOutput:
     comments = []
 
     if not repo.branches.has_branch("STU"):
-        comments.append(BRANCH_NOT_CREATED.format(branch="STU"))
+        comments.append(BRANCH_MISSING.format(branch="STU"))
     else:
-        stu_branch = repo.branches.branch("STU").branch
-        tracking = stu_branch.tracking_branch()
-        if not tracking or tracking.name != 'origin/STU':
+        try:
+            exercise.repo.repo.refs["origin/STU"]
+        except (IndexError, KeyError):
             comments.append(BRANCH_NOT_TRACKING.format(branch="STU"))
+            pass 
 
     if not repo.branches.has_branch("VWX"):
-        comments.append(BRANCH_NOT_CREATED.format(branch="VWX"))
+        comments.append(BRANCH_MISSING.format(branch="VWX"))
     else:
-        vwx_branch = repo.branches.branch("VWX").branch
-        tracking = vwx_branch.tracking_branch()
-        if not tracking or tracking.name != 'origin/VWX':
+        try:
+            exercise.repo.repo.refs["origin/VWX"]
+        except (IndexError, KeyError):
             comments.append(BRANCH_NOT_TRACKING.format(branch="VWX"))
-            
+            pass 
+
     if not repo.branches.has_branch("ABC"):
         comments.append(BRANCH_MISSING.format(branch="ABC"))
     else:
-        abc_branch = repo.branches.branch("ABC").branch
-        remote_abc = abc_branch.tracking_branch()
-        if remote_abc:
-            local_commits = set(commit.hexsha for commit in repo.branches.branch("ABC").commits)
-            remote_commit_hexsha = remote_abc.commit.hexsha
-            if remote_commit_hexsha not in local_commits:
-                comments.append(COMMIT_MISSING.format(branch="ABC"))
-        else:
+        try:
+            remote_abc = exercise.repo.repo.refs["origin/ABC"]
+            abc_commits = repo.branches.branch("ABC").commits
+            if not get_commit_from_hexsha(abc_commits, remote_abc.commit.hexsha):
+                comments.append(REMOTE_COMMIT_MISSING.format(branch="ABC"))
+        except (IndexError, KeyError):
             comments.append(BRANCH_NOT_TRACKING.format(branch="ABC"))
-
+            pass 
+            
     if not repo.branches.has_branch("DEF"):
         comments.append(BRANCH_MISSING.format(branch="DEF"))
     else:
-        def_branch = repo.branches.branch("DEF").branch
-        remote_def = def_branch.tracking_branch()
-        if remote_def:
-            local_commits = repo.branches.branch("DEF").commits
-            remote_commit_hexsha = remote_def.commit.hexsha
-            if remote_commit_hexsha not in set(commit.hexsha for commit in local_commits):
-                comments.append(COMMIT_MISSING.format(branch="DEF"))
-            if not get_commit_from_message(local_commits, "Add 'documentation'"):
+        try:
+            remote_def = exercise.repo.repo.refs["origin/DEF"]
+            def_commits = repo.branches.branch("DEF").commits
+            if not get_commit_from_hexsha(def_commits, remote_def.commit.hexsha):
+                comments.append(REMOTE_COMMIT_MISSING.format(branch="DEF"))
+            if not get_commit_from_message(def_commits, "Add 'documentation'"):
                 comments.append(LOCAL_COMMIT_MISSING)
-        else:
+        except (IndexError, KeyError):
             comments.append(BRANCH_NOT_TRACKING.format(branch="DEF"))
+            pass 
 
     if comments:
         raise exercise.wrong_answer(comments)
