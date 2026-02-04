2e80024581e8a55890846e159b9882c623d30a02
jovnc <95868357+jovnc@users.noreply.github.com>
2026-02-04 23:06:00 +0800

    chore: remove unnecessary import and format code

--- glossary_branch_rename/test_verify.py
+++ glossary_branch_rename/test_verify.py
@@ -1,12 +1,15 @@
 from contextlib import contextmanager
-import os
 from typing import Iterator, Tuple
 
 from git_autograder import GitAutograderStatus
-from exercise_utils.test import GitAutograderTest, GitAutograderTestLoader, assert_output
+from exercise_utils.test import (
+    GitAutograderTest,
+    GitAutograderTestLoader,
+    assert_output,
+)
 from repo_smith.repo_smith import RepoSmith
 
-from .verify import(
+from .verify import (
     verify,
     STU_LOCAL_PRESENT,
     STU_REMOTE_PRESENT,
@@ -21,33 +24,36 @@ BRANCH_TO_RENAME = "STU"
 
 loader = GitAutograderTestLoader(REPOSITORY_NAME, verify)
 
+
 @contextmanager
 def base_setup() -> Iterator[Tuple[GitAutograderTest, RepoSmith]]:
     with loader.start(include_remote_repo=True) as (test, rs, rs_remote):
         remote_worktree_dir = rs_remote.repo.working_tree_dir
-        
+
         # set up local repo
         rs.git.commit(message="Empty", allow_empty=True)
         rs.git.remote_add("origin", str(remote_worktree_dir))
-        
+
         for remote_branch_name in BRANCHES:
             rs_remote.git.branch(remote_branch_name)
 
         rs.git.push("origin", all=True)
-        
+
         yield test, rs
+
 
 def test_base():
     with base_setup() as (test, rs):
         rs.git.branch(EXPECTED_NEW_BRANCH_NAME, old_branch=BRANCH_TO_RENAME, move=True)
         rs.git.push("origin", EXPECTED_NEW_BRANCH_NAME)
         rs.git.push("origin", f":{BRANCH_TO_RENAME}")
-        
+
         output = test.run()
         assert_output(output, GitAutograderStatus.SUCCESSFUL)
 
+
 def test_no_change():
-    with base_setup() as (test, rs):
+    with base_setup() as (test, _):
         output = test.run()
         assert_output(
             output,
@@ -56,9 +62,10 @@ def test_no_change():
                 STU_LOCAL_PRESENT,
                 RENAMED_LOCAL_MISSING,
                 STU_REMOTE_PRESENT,
-                RENAMED_REMOTE_MISSING
-            ]
+                RENAMED_REMOTE_MISSING,
+            ],
         )
+
 
 def test_changed_local_only():
     with base_setup() as (test, rs):
@@ -67,12 +74,10 @@ def test_changed_local_only():
         assert_output(
             output,
             GitAutograderStatus.UNSUCCESSFUL,
-            [
-                STU_REMOTE_PRESENT,
-                RENAMED_REMOTE_MISSING
-            ]
+            [STU_REMOTE_PRESENT, RENAMED_REMOTE_MISSING],
         )
-        
+
+
 def test_changed_local_wrong_name():
     with base_setup() as (test, rs):
         rs.git.branch("S-to-X", old_branch=BRANCH_TO_RENAME, move=True)
@@ -84,8 +89,9 @@ def test_changed_local_wrong_name():
                 RENAMED_LOCAL_MISSING,
                 STU_REMOTE_PRESENT,
                 RENAMED_REMOTE_MISSING,
-            ]
+            ],
         )
+
 
 def test_changed_remote_wrong_name():
     with base_setup() as (test, rs):
@@ -99,9 +105,10 @@ def test_changed_remote_wrong_name():
             [
                 RENAMED_LOCAL_MISSING,
                 RENAMED_REMOTE_MISSING,
-             ]
+            ],
         )
-        
+
+
 def test_local_old_branch_still_exists():
     with base_setup() as (test, rs):
         rs.git.branch(EXPECTED_NEW_BRANCH_NAME)
@@ -109,7 +116,8 @@ def test_local_old_branch_still_exists():
         rs.git.push("origin", f":{BRANCH_TO_RENAME}")
 
         output = test.run()
-        assert_output(output, GitAutograderStatus.UNSUCCESSFUL,[STU_LOCAL_PRESENT])
+        assert_output(output, GitAutograderStatus.UNSUCCESSFUL, [STU_LOCAL_PRESENT])
+
 
 def test_remote_old_branch_still_exists():
     with base_setup() as (test, rs):
