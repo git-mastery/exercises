d6a3e84edd831fc9de59431a0e35e5470102b3f9
jovnc <95868357+jovnc@users.noreply.github.com>
2026-03-01 15:36:30 +0800

    [tooling] Address copilot comments for LeftHook

--- setup.sh
+++ setup.sh
@@ -8,6 +8,8 @@ source venv/bin/activate
 
 pip install -r requirements.txt -U --no-cache-dir
 
-if ! command -v lefthook &>/dev/null; then
+if command -v lefthook  >/dev/null 2>&1; then
   lefthook install
+else
+  echo "LeftHook not installed, failed to set up LeftHook for Git Hooks."
 fi
