d7a8ec3bd12e77ea7d1b28435709bb54be680eda
jovnc <jovnc@users.noreply.github.com>
2026-03-31 15:48:34 +0000

    deploy: ff8ec6d2a5029a8390b0b0fa5b3825d17f9b5c21

--- setup.sh
+++ setup.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
 
-if [ ! -d venv/ ]; then
-  python -m venv venv
-fi
-
-source venv/bin/activate
-
-pip install -r requirements.txt -U --no-cache-dir
-
-if command -v lefthook  >/dev/null 2>&1; then
-  lefthook install
-else
-  echo "LeftHook not installed, failed to set up LeftHook for Git Hooks."
-fi
+uv sync --all-groups
+uv run lefthook install
