e462b434d4810da73fedafd0656e407b02b2e21a
desmondwong1215 <desmondwong1215@gmail.com>
2026-04-06 23:46:43 +0800

    Update test-verify

--- create_pr_from_main/test_verify.py
+++ create_pr_from_main/test_verify.py
@@ -1,13 +1,9 @@
-import json
 from contextlib import contextmanager
-from pathlib import Path
 from unittest.mock import PropertyMock, patch
 
 import pytest
-from exercise_utils.test import assert_output
-from git.repo import Repo
+from exercise_utils.test import GitAutograderTestLoader, assert_output
 from git_autograder import (
-    GitAutograderExercise,
     GitAutograderStatus,
     GitAutograderWrongAnswerException,
 )
@@ -22,41 +18,12 @@ from .verify import (
     verify,
 )
 
+REPOSITORY_NAME = "create-pr-from-main"
 
-@pytest.fixture
-def exercise(tmp_path: Path) -> GitAutograderExercise:
-    repo_dir = tmp_path / "ignore-me"
-    repo_dir.mkdir()
-    Repo.init(repo_dir)
+loader = GitAutograderTestLoader(REPOSITORY_NAME, verify)
 
-    with open(tmp_path / ".gitmastery-exercise.json", "a") as config_file:
-        config_file.write(
-            json.dumps(
-                {
-                    "exercise_name": "create_pr_from_main",
-                    "tags": [],
-                    "requires_git": True,
-                    "requires_github": True,
-                    "base_files": {},
-                    "exercise_repo": {
-                        "repo_type": "local",
-                        "repo_name": "ignore-me",
-                        "init": True,
-                        "create_fork": None,
-                        "repo_title": "gm-shapes",
-                        "pr_number": 1,
-                        "pr_repo_full_name": "dummy/repo",
-                    },
-                    "downloaded_at": None,
-                }
-            )
-        )
-
-    with patch(
-        "git_autograder.pr.fetch_pull_request_data",
-        return_value={},
-    ):
-        return GitAutograderExercise(exercise_path=tmp_path)
+# NOTE: This exercise is a special case where we do not require repo-smith. Instead,
+# we directly mock function calls to verify that all branches are covered for us.
 
 
 class FakeCommit:
@@ -69,39 +36,42 @@ class FakeCommit:
 
 
 def _run_verify(
-    exercise: GitAutograderExercise,
     pr_numbers: list[int] = [],
     head_branch: str = "",
     java_content: str | None = None,
 ):
     fake_commit = FakeCommit(java_content)
-    with (
-        patch("create_pr_from_main.verify.get_github_username", return_value="dummy"),
-        patch(
-            "create_pr_from_main.verify.get_pr_numbers_by_author",
-            return_value=pr_numbers,
-        ),
-        patch("create_pr_from_main.verify.add_pr_config"),
-        patch.object(exercise, "fetch_pr", return_value=None),
-        patch.object(
-            GitAutograderPr,
-            "head_branch",
-            new_callable=PropertyMock,
-            return_value=head_branch,
-        ),
-        patch.object(
-            GitAutograderPr,
-            "last_user_commit",
-            new_callable=PropertyMock,
-            return_value=fake_commit,
-        ),
-    ):
-        return verify(exercise)
+    with loader.start_mock_exercise(
+        has_pr_context=True, 
+        pr_number=1, 
+        pr_repo_full_name="dummy/repo"
+    ) as exercise:
+        with (
+            patch("create_pr_from_main.verify.get_github_username", return_value="dummy"),
+            patch(
+                "create_pr_from_main.verify.get_pr_numbers_by_author",
+                return_value=pr_numbers,
+            ),
+            patch("create_pr_from_main.verify.add_pr_config"),
+            patch.object(exercise, "fetch_pr", return_value=None),
+            patch.object(
+                GitAutograderPr,
+                "head_branch",
+                new_callable=PropertyMock,
+                return_value=head_branch,
+            ),
+            patch.object(
+                GitAutograderPr,
+                "last_user_commit",
+                new_callable=PropertyMock,
+                return_value=fake_commit,
+            ),
+        ):
+            return verify(exercise)
 
 
-def test_success(exercise: GitAutograderExercise):
+def test_success():
     output = _run_verify(
-        exercise,
         pr_numbers=[123],
         head_branch="main",
         java_content="\n".join(EXPECTED_CONTENT_STEP_3),
@@ -110,17 +80,16 @@ def test_success(exercise: GitAutograderExercise):
     assert_output(output, GitAutograderStatus.SUCCESSFUL)
 
 
-def test_pr_missing(exercise: GitAutograderExercise):
+def test_pr_missing():
     with pytest.raises(GitAutograderWrongAnswerException) as exception:
-        _run_verify(exercise)
+        _run_verify()
 
     assert exception.value.message == [PR_MISSING]
 
 
-def test_wrong_head_branch(exercise: GitAutograderExercise):
+def test_wrong_head_branch():
     with pytest.raises(GitAutograderWrongAnswerException) as exception:
         _run_verify(
-            exercise,
             pr_numbers=[1],
             head_branch="feature/pr-branch"
         )
@@ -128,10 +97,9 @@ def test_wrong_head_branch(exercise: GitAutograderExercise):
     assert exception.value.message == [WRONG_HEAD_BRANCH]
 
 
-def test_java_file_missing(exercise: GitAutograderExercise):
+def test_java_file_missing():
     with pytest.raises(GitAutograderWrongAnswerException) as exception:
         _run_verify(
-            exercise,
             pr_numbers=[1],
             head_branch="main",
             java_content=None,
@@ -140,10 +108,9 @@ def test_java_file_missing(exercise: GitAutograderExercise):
     assert exception.value.message == [JAVA_FILE_MISSING]
 
 
-def test_java_content_invalid(exercise: GitAutograderExercise):
+def test_java_content_invalid():
     with pytest.raises(GitAutograderWrongAnswerException) as exception:
         _run_verify(
-            exercise,
             pr_numbers=[1],
             head_branch="main",
             java_content="wrong content\n",
